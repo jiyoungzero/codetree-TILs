e15ae79bd43d75d52f7e80f49ef6645dd2ffd279
Jiyoung Lee <79441145+jiyoungzero@users.noreply.github.com>
2023-11-08 23:41:43 +0900

    Title: 사다리 타기, Time: 227ms, Memory: 27MB, Status: Passed - CodeTree

--- 231108/사다리 타기/ladder-game.py
+++ 231108/사다리 타기/ladder-game.py
@@ -1,55 +1,48 @@
 n, m = map(int, input().split())
+lines = []
+for _ in range(m):
+    a, b = tuple(map(int, input().split()))
+    lines.append((b, a - 1))
 
-lines = [tuple(map(int, input().split())) for _ in range(m)]
-arr = [i for i in range(1, n+1)]
+lines.sort()
 
-orginal_result = arr[::]
-answer = int(1e9)
-
-def swap(lst2, idx):
-    tmp = lst2[idx]
-    lst2[idx] = lst2[idx+1]
-    lst2[idx+1] = tmp
-    return lst2
-
+answer = m
+path = []
 
 # 사다리 타기 결과 
-def get_result(lst, cmds):
-    cmds.sort(key = lambda x : (x[1], x[0]))
-    for cmd in cmds:
-        a, b = cmd
-        a -= 1
-        lst = swap(lst, a)
-    return lst
+def is_same():
+    num1, num2 = [i for i in range(n)], [i for i in range(n)]
 
-def is_same(arr1, arr2):
-    for e1, e2 in zip(arr1, arr2):
+    for _, idx in lines: # 원래 결과
+        num1[idx], num1[idx+1] = num1[idx+1], num1[idx]
+    for _, idx in path: # 선택한 결과
+        num2[idx], num2[idx+1] = num2[idx+1], num2[idx]
+
+    for e1, e2 in zip(num1, num2):
         if e1 != e2:
             return False
     return True
 
-# 원래 결과
-orginal_result = get_result(orginal_result, lines)
+
 
 
 # 선의 개수를 0개부터 시작해서 m개까지 늘려가면서 원래 결과와 같으면 return 
-def backtracking(depth, path):
-    global answer, arr
-    new_arr = arr[::]
-    if depth >= m:
-        return 
-    if is_same(orginal_result, get_result(new_arr,path)):
-        answer = min(answer, len(path))
+def backtracking(depth):
+    global answer
+
+    if depth >= m: # 오류 원인 : 선택 안하는 거까지 가야 하니까 depth을 끝까지 돌려줘야 함.!!!
+        if is_same():
+            answer = min(answer, len(path))
         return 
 
     path.append(lines[depth])
-    backtracking(depth+1, path)
+    backtracking(depth+1)
     path.pop()
 
-    backtracking(depth+1, path)
+    backtracking(depth+1)
 
 if m > 1:
-    backtracking(0, [])
+    backtracking(0)
     print(answer)
 else:
     print(1)
