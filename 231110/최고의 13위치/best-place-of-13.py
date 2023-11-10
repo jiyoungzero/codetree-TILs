8ba520a64b3e4d949d03bebb944b13225ca65133
Jiyoung Lee <79441145+jiyoungzero@users.noreply.github.com>
2023-11-10 13:06:07 +0900

    Title: 최고의 13위치, Time: 225ms, Memory: 24MB, Status: Passed - CodeTree

--- 231110/최고의 13위치/best-place-of-13.py
+++ 231110/최고의 13위치/best-place-of-13.py
@@ -2,12 +2,10 @@ import sys
 input = sys.stdin.readline
 
 n = int(input())
-arr = [list(map(int, input().split()))]
+arr = [list(map(int, input().split())) for _ in range(n)]
 answer = 0
 
 for i in range(n):
     for j in range(n-2):
-        tmp = arr[i][j]+arr[i][j+1]+arr[i][j+2]
-        print(tmp)
-        answer = max(answer, tmp.count(1))
+        answer = max(answer,  arr[i][j]+arr[i][j+1]+arr[i][j+2])
 print(answer)
