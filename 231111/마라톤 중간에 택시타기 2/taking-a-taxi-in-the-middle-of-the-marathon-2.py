7162facbfc0cb4723d692e561fac1eb6ebed282f
Jiyoung Lee <79441145+jiyoungzero@users.noreply.github.com>
2023-11-11 14:00:05 +0900

    Title: 마라톤 중간에 택시타기 2, Time: 106ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 231111/마라톤 중간에 택시타기 2/taking-a-taxi-in-the-middle-of-the-marathon-2.py
@@ -0,0 +1,17 @@
+n = int(input())
+checkpt = [tuple(map(int, input().split())) for _ in range(n)]
+
+answer = int(1e9)
+
+def get_distance(arr):
+    result = 0
+    prevx, prevy = arr[0][0], arr[0][1]
+    for i in range(1, n-1):
+        result += (abs(prevx-arr[i][0])+ abs(prevy-arr[i][1]))
+    return result
+
+for i in range(n):
+    copy_ckp = checkpt[:]
+    del copy_ckp[i]
+    answer = min(answer, get_distance(copy_ckp))
+print(answer)
