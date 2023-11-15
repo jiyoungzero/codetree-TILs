2b42cfff80a119305ca49749cc7784fb45d16c0b
Jiyoung Lee <79441145+jiyoungzero@users.noreply.github.com>
2023-11-15 17:33:25 +0900

    Title: 두 수의 합, Time: 205ms, Memory: 38MB, Status: Passed - CodeTree

--- 231115/두 수의 합/sum-of-two-num.py
+++ 231115/두 수의 합/sum-of-two-num.py
@@ -1,21 +1,17 @@
 n, k = map(int, input().split())
 arr = list(map(int, input().split()))
+# 답안
+
+count = dict()
+
 answer = 0
-numDict = {}
+for ele in arr:
+    diff = k - ele
+    if diff in count:
+        answer += count[diff]
 
-for i in range(n):
-    if arr[i] in numDict.keys():
-        numDict[arr[i]] += 1
-    else: numDict[arr[i]] =1
-
-setNum = list(set(arr))
-
-for i in range(len(setNum)):
-    if (setNum[i] == (k-setNum[i])) and setNum[i] in numDict.keys():
-        answer += ((numDict[setNum[i]]*(numDict[setNum[i]]-1)))
+    if ele in count:
+        count[ele] += 1
     else:
-        if k-setNum[i] in numDict.keys():
-            answer += ((numDict[setNum[i]])*numDict[k-setNum[i]])
-    #print(answer,((numDict[setNum[i]]*(numDict[setNum[i]]-1))//2))
-
-print(answer//2)
+        count[ele] = 1
+print(answer)
