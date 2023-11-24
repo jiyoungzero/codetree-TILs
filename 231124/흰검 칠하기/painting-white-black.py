578c8d3939168c2ebb4fec4698fa04b29bab4866
Jiyoung Lee <79441145+jiyoungzero@users.noreply.github.com>
2023-11-24 14:20:43 +0900

    Title: 흰검 칠하기, Time: 242ms, Memory: 39MB, Status: Passed - CodeTree

--- 231124/흰검 칠하기/painting-white-black.py
+++ 231124/흰검 칠하기/painting-white-black.py
@@ -4,25 +4,34 @@ import sys
 input = sys.stdin.readline 
 
 n = int(input())
-arr = [[0,0,0] for _ in range(200002)]
+arr = [[0,0,0] for _ in range(200002)] # [w,b, now_color]
 start = 100000
 w, b,g = 0,0,0
 
 for _ in range(n):
     x, cmd = map(str, input().split())
-    if cmd == 'L': # 흰색 
+    if cmd == 'L': # 흰색 = 0
         for i in range(start, start-int(x), -1):
             arr[i][0] += 1
-        start -= int(x)
-    else:
+            arr[i][2] = 'w'
+
+        start -= (int(x)-1)
+
+    else: # 검은색 = 1
         for i in range(start, start+int(x)):
             arr[i][1] += 1
-        start += int(x)
+            arr[i][2] = 'b'
+        start += (int(x)-1)
+
+
 
 for i in range(len(arr)):
     if arr[i][0] >=2 and arr[i][1] >= 2:
         g += 1
-    elif arr[i][0] > 0:
-        w += 1
     else:
-        b += 1
+        if arr[i][2] == 'w':
+            w += 1
+        elif arr[i][2] == 'b':
+            b += 1
+
+print(w, b, g)
