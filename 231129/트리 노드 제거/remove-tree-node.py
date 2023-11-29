1deb7f2692d8ef4aafd9a3af8f26be7e2ba46bae
Jiyoung Lee <79441145+jiyoungzero@users.noreply.github.com>
2023-11-29 17:57:01 +0900

    Title: 트리 노드 제거, Time: 111ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 231129/트리 노드 제거/remove-tree-node.py
+++ 231129/트리 노드 제거/remove-tree-node.py
@@ -5,49 +5,28 @@ input = sys.stdin.readline
 n = int(input())
 parent = list(map(int, input().split()))
 del_node = int(input())
-tree = [[] for _ in range(n)]
+leaf = [0]*n 
+root = 0
+graph = [[] for _ in range(n)]
+
+for i in range(n):
+    graph[parent[i]].append(i)
+
+def dfs(x, parent):
+    global leaf
+    if not graph[x]:
+        leaf[x] += 1
+    for y in graph[x]:
+        dfs(y, x)
+        leaf[x] += leaf[y]
 
 
-# parent토대로 트리 만들어 보기 
-for child, p in enumerate(parent):
-    if p == -1:continue
-    elif child == p:continue
-    else:tree[p].append(child)
-
-
-
-
-# 삭제하고자 하는 본인노드 자식 + 그의 자식까지 지우기
-# del tree[del_node]
-del_lst = [del_node] # 삭제해야 할 노드들
-delAll = False 
-
-while not delAll:
-    tmp = []
-    delAll = True 
-    for node in del_lst:
-        if len(tree[node]) != 0:
-            delAll = False
-    if delAll:
-        break
+def solution():
+    for i in range(n):
+        if del_node in graph[i]:
+            graph[i].remove(del_node)
     
-    while del_lst:
-        node = del_lst.pop()
-        tmp = tree[node]
-        for t in tree:
-            if node in t:
-                t.remove(node)
-        if tree[node] : 
-            tree[node] = [] 
-    del_lst = tmp
-
-
-    
-
-answer = 0
-for t in tree:
-    answer += (len(t))
-
-
-
-print(answer)
+    if root != del_node:
+        dfs(root, -1)
+solution()
+print(leaf[root])
