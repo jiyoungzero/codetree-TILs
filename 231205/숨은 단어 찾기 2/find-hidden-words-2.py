cf7b121a0819c25019a42461463f6a5484a82741
Jiyoung Lee <79441145+jiyoungzero@users.noreply.github.com>
2023-12-05 17:53:58 +0900

    Title: 숨은 단어 찾기 2, Time: 104ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 231205/숨은 단어 찾기 2/find-hidden-words-2.py
+++ 231205/숨은 단어 찾기 2/find-hidden-words-2.py
@@ -5,6 +5,7 @@ input =sys.stdin.readline
 
 n,m = map(int, input().split())
 arr = []
+answer = 0
 
 for _ in range(n):
     arr.append(input().rstrip())
@@ -17,11 +18,32 @@ verti = [[(-2, 0),(-1, 0),(0,0)], [(-1,0), (0,0),(1,0)], [(0,0), (1,0),(2,0)]]
 r_diag = [[(-2,-2),(-1,-1),(0,0)], [(-1,-1), (0,0),(1,1)], [(0,0), (1,1),(2,2)]]
 # 왼쪽 대각선
 l_diag = [[(-2, 2),(-1,1),(0,0)], [(-1,1), (0,0),(1,-1)], [(0,0), (1,-1),(2,-2)]]
+checked = []
 
 def in_range(x, y):
     return 0 <= x < n and 0<= y <m
+
+def search(lst):
+    global checked
+    global answer
+    for jump in horiz:
+        tmp = ''
+        tmp_check = []
+        for ji,jj in jump:
+            ni, nj = i+ji, j+jj
+            if in_range(ni, nj):
+                tmp += arr[ni][nj]
+                tmp_check.append([ni,nj])
+            if len(tmp) == 3 and tmp_check not in checked and (tmp == 'LEE' or tmp == 'EEL'):
+                answer += 1
+                checked.append(tmp_check)
+                print(tmp, tmp_check)
+
 for i in range(n):
     for j in range(m):
-        for case in horiz:
-    
-            ni, nj = i+case
+        search(horiz)
+        search(verti)
+        search(r_diag)
+        search(l_diag)
+
+print(answer)
