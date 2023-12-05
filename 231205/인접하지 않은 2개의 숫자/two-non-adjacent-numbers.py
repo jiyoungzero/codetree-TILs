6741f279a1751e7130cc86bbff33830da3c7cb4c
Jiyoung Lee <79441145+jiyoungzero@users.noreply.github.com>
2023-12-05 15:25:22 +0900

    Title: 인접하지 않은 2개의 숫자, Time: 111ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 231205/인접하지 않은 2개의 숫자/two-non-adjacent-numbers.py
@@ -0,0 +1,17 @@
+import sys
+input =sys.stdin.readline
+
+
+
+n = int(input())
+arr = list(map(int, input().split()))
+answer = 0
+
+for i in range(n):
+    tmp = 0
+    for j in range(i+2,n):
+        tmp = arr[i] + arr[j]
+    answer = max(answer, tmp)
+
+
+print(answer)
