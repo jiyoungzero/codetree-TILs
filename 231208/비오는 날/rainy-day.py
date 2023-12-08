c870d6865145696dee6f8039635bf72be3135f02
Jiyoung Lee <79441145+jiyoungzero@users.noreply.github.com>
2023-12-08 14:16:28 +0900

    Title: 비오는 날, Time: 117ms, Memory: 24MB, Status: Passed - CodeTree

--- 231208/비오는 날/rainy-day.py
+++ 231208/비오는 날/rainy-day.py
@@ -7,8 +7,8 @@ commands = [input().rstrip() for _ in range(n)]
 
 
 def strToDate(date):
-    year, month, day = date[:4], date[6:8], date[9:]
-    return year*12*30 + month*30 + day
+    year, month, day = date.split('-')
+    return int(year)*12*30 + int(month)*30 + int(day)
 
 rainy = []
 for idx, command in enumerate(commands):
@@ -16,5 +16,8 @@ for idx, command in enumerate(commands):
     if weather == 'Rain': rainy.append([strToDate(date), idx])
     else:continue
 
+
 rainy.sort()
+
+
 print(commands[rainy[0][1]])
