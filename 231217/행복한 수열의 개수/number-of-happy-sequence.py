c746b27d5e01d17e91bbe0b3f1bda259ad26d4ef
Jiyoung Lee <79441145+jiyoungzero@users.noreply.github.com>
2023-12-17 22:48:56 +0900

    Title: 행복한 수열의 개수, Time: 126ms, Memory: 25MB, Status: Wrong Answer - CodeTree

--- 231217/행복한 수열의 개수/number-of-happy-sequence.py
+++ 231217/행복한 수열의 개수/number-of-happy-sequence.py
@@ -11,12 +11,12 @@ answer = 0
 # 행 
 for i in range(n):
     start = arr[i][0]
-    tmp = 0
+    tmp = 1
     for j in range(1, n):
         if start == arr[i][j]:
             tmp += 1
         else:
-            tmp = 0
+            tmp = 1
         start = arr[i][j]
     if tmp >= m:
         answer += 1
@@ -26,12 +26,12 @@ for i in range(n):
 # 열
 for j in range(n):
     start = arr[0][j]
-    tmp = 0
+    tmp = 1
     for i in range(1, n):
         if start == arr[i][j]:
             tmp += 1
         else:
-            tmp = 0
+            tmp = 1
         start = arr[i][j]
     if tmp >= m:
         answer += 1
