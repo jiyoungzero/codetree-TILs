0032f3c80800fd11875a1b6a8c247da3c7a373a8
Jiyoung Lee <79441145+jiyoungzero@users.noreply.github.com>
2023-12-22 23:58:03 +0900

    Title: 자연수 M/2개의 쌍, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 231222/자연수 M/2개의 쌍/m2-pairs-of-natural-numbers.py
+++ 231222/자연수 M/2개의 쌍/m2-pairs-of-natural-numbers.py
@@ -2,15 +2,29 @@ import sys
 input =sys.stdin.readline
 
 n = int(input())
-nums = []
+nums_cnt = []
 for _ in range(n):
     x, y = map(int, input().split())
-    for _ in range(x):
-        nums.append(y)
-m = len(nums)
+    nums_cnt.append([y, x])
+m = len(nums_cnt)
+nums_cnt.sort(key=lambda x:(x[0]))
 answer = 0
+visited = [False]*n
+i, j = 0, m-1
 
-nums.sort()
-for i in range(m//2):
-    answer = max(answer, nums[i]+nums[m-i-1])
+while 1:
+    if all(visited):
+        break
+    t = min(nums_cnt[i][1], nums_cnt[j][1])
+    if i == j:
+        t //= 2
+    answer = max(answer, nums_cnt[i][0] + nums_cnt[j][0])
+    nums_cnt[i][1] -= t
+    nums_cnt[j][1] -= t
+    if nums_cnt[i][1] <= 0:
+        i += 1
+        visited[i] = True
+    if nums_cnt[j][1] <= 0:
+        j -= 1
+        visited[j] = True
 print(answer)
