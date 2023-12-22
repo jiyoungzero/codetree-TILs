c6a09786eb590c491620f786558b480672fcd5af
Jiyoung Lee <79441145+jiyoungzero@users.noreply.github.com>
2023-12-23 00:04:10 +0900

    Title: 자연수 M/2개의 쌍, Time: 377ms, Memory: 31MB, Status: Passed - CodeTree

--- 231223/자연수 M/2개의 쌍/m2-pairs-of-natural-numbers.py
+++ 231223/자연수 M/2개의 쌍/m2-pairs-of-natural-numbers.py
@@ -9,7 +9,6 @@ for _ in range(n):
 m = len(nums_cnt)
 nums_cnt.sort(key=lambda x:(x[0]))
 answer = 0
-visited = [False]*n
 i, j = 0, m-1
 
 while 1:
@@ -22,12 +21,10 @@ while 1:
     nums_cnt[i][1] -= t
     nums_cnt[j][1] -= t
     if nums_cnt[i][1] <= 0:
-        visited[i] = True
         i += 1
         i = min(m, i)
         
     if nums_cnt[j][1] <= 0:
-        visited[j] = True
         j -= 1
         j = max(0, j)
 
