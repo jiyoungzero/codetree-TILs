fa8b3e09be70fcddd39936c5938782f53b1ad0bc
Jiyoung Lee <79441145+jiyoungzero@users.noreply.github.com>
2023-12-24 00:32:52 +0900

    Title: 적석과 흑석 2, Time: 114ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 231224/적석과 흑석 2/red-stone-and-black-stone-2.py
@@ -0,0 +1,17 @@
+import sys
+input = sys.stdin.readline
+
+c, n = map(int, input().split())
+red_stones = [int(input()) for _ in range(c)]
+black_stones = [tuple(map(int, input().split())) for _ in range(n)]
+answer = 0
+
+red_stones.sort(reverse = True)
+black_stones.sort(reverse = True)
+for r in red_stones:
+    for c in black_stones:
+        a, b = c
+        if a <= r <= b:
+            answer += 1
+            black_stones.remove(c)
+print(answer)
