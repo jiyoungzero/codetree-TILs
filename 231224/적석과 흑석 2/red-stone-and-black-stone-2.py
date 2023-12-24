89ce9ced7f61718a0df41b982fbc103a4cbdff2c
Jiyoung Lee <79441145+jiyoungzero@users.noreply.github.com>
2023-12-24 23:50:14 +0900

    Title: 적석과 흑석 2, Time: 5008ms, Memory: 38MB, Status: Time Limit Exceed - CodeTree

--- 231224/적석과 흑석 2/red-stone-and-black-stone-2.py
+++ 231224/적석과 흑석 2/red-stone-and-black-stone-2.py
@@ -1,34 +1,26 @@
-from bisect import bisect_left
+import sys
+input = sys.stdin.readline
+from sortedcontainers import SortedDict
 
 c, n = map(int, input().split())
-red = [int(input()) for _ in range(c)]
-black = [list(map(int, input().split())) for _ in range(n)]
+red_tmp = [int(input()) for _ in range(c)]
+black_stones = [tuple(map(int, input().split())) for _ in range(n)]
+answer = 0
 
-red.sort()
-black = sorted(black, key=lambda x: x[1])
+red_stones = SortedDict()
+for i in range(c):
+    red_stones[red_tmp[i]] = False
+# red_stones = dict(red_stones)
+# print(red_stones)
 
-ans = 0
-visited = set()
-for i in range(n):
-    s, e = black[i]
-    if s > red[-1]: continue
-    if e < red[0]: continue
+for j in range(n):
+    s, e = black_stones[j]
 
-    s_idx, e_idx = bisect_left(red, s), bisect_left(red, e)
+    for k, v in red_stones.items():
+        if s <= k <= e and v == False:
+            answer += 1
+            red_stones[k] = True
+            break
 
-    if e_idx == len(red): e_idx -= 1
-    if s_idx == len(red): s_idx -= 1
 
-    # print(i, s_idx, e_idx)
-    # while s_idx <= e_idx and red[s_idx] in visited:
-    while s_idx < len(red) and red[s_idx] in visited and s_idx <= e_idx:
-        s_idx += 1
-    
-    if s_idx > e_idx or red[s_idx] > e: # 못찾음
-        continue
-    else:
-        # print(i, red[s_idx], visited)
-        ans += 1
-        visited.add(red[s_idx])
-
-print(ans)
+print(answer)
