3ebfadd5bb487de699c7197c632c7a0970ca682d
Jiyoung Lee <79441145+jiyoungzero@users.noreply.github.com>
2023-12-25 00:10:45 +0900

    Title: 적석과 흑석 2, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 231225/적석과 흑석 2/red-stone-and-black-stone-2.py
@@ -0,0 +1,24 @@
+import sys
+input = sys.stdin.readline
+from sortedcontainers import SortedSet
+
+c, n = map(int, input().split())
+red_tmp = [int(input()) for _ in range(c)]
+black_stones = []
+for _ in range(n):
+    a, b = tuple(map(int, input().split()))
+    black_stones.append((b, a))
+answer = 0
+
+red_stones = SortedSet(red_tmp)
+
+black_stones.sort()
+
+for j in range(n):
+    e, s = black_stones[j]
+    t_idx = red_stones.bisect_left(s)
+    if t_idx != c and red_stones[t_idx] <= e:
+        answer += 1
+        red_stones.remove(red_stones[t_idx])
+
+print(answer)
