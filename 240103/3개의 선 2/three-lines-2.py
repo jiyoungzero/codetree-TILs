8d3c8c6330f48a28b9870993b8d47bb41379f523
Jiyoung Lee <79441145+jiyoungzero@users.noreply.github.com>
2024-01-03 00:40:41 +0900

    Title: 3개의 선 2, Time: 171ms, Memory: 25MB, Status: Passed - CodeTree

--- 240103/3개의 선 2/three-lines-2.py
+++ 240103/3개의 선 2/three-lines-2.py
@@ -4,26 +4,48 @@ from collections import defaultdict
 
 n = int(input())
 points = [tuple(map(int, input().split())) for _ in range(n)]
-line_cnt = 0 
-visited = [False]*n
+answer = 0
+MAX_VALUE = 0
 
-for i in range(n):
-    x, y = points[i]
-    flag = False
-    if visited[i]:continue
-    if i == n-1 and not visited[i]:
-        line_cnt += 1
+for x, y in points:
+    MAX_VALUE = max([x, y, MAX_VALUE])
 
-    for j in range(i+1, n):
-        a, b = points[j]
-        if visited[j]:continue
-        if not visited[j] and (x == a or b == y):
+
+for i in range(MAX_VALUE+1):
+    for j in range(MAX_VALUE+1):
+        for k in range(MAX_VALUE+1):
             flag = True
-            visited[j] = True
-    if flag:
-        line_cnt += 1
 
-if line_cnt == 3:
-    print(1)
-else:
-    print(0)
+            # x축으로만 3개
+            for x, y in points:
+                if i == y or j == y or k == y:
+                    continue
+                flag = False
+            if flag:
+                answer = 1 
+
+            flag = True
+            # x축 2개, y축 1개
+            for x, y in points:
+                if (i == y or j == y) or k == x:
+                    continue
+                flag = False
+            if flag:
+                answer = 1   
+            flag = True
+            # x축 1개, y축 2개
+            for x, y in points:
+                if i == y or (j == x or k == x):
+                    continue
+                flag = False
+            if flag:
+                answer = 1                           
+            flag = True
+            # y축 3개
+            for x, y in points:
+                if i == x or j == x or k == x:
+                    continue
+                flag = False
+            if flag:
+                answer = 1   
+print(answer)
