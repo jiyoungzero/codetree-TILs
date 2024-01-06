f7fef932b107675d1ace057fc31c7e3e6adca8a2
Jiyoung Lee <79441145+jiyoungzero@users.noreply.github.com>
2024-01-06 22:57:10 +0900

    Title: 네 방향 탈출 가능 여부 판별하기, Time: 1492ms, Memory: 288MB, Status: Memory Limit Exceed - CodeTree

---
+++ 240106/네 방향 탈출 가능 여부 판별하기/determine-escapableness-with-4-ways.py
@@ -0,0 +1,43 @@
+import sys
+input = sys.stdin.readline
+from collections import deque
+
+BLANK = 1
+SNAKE = 0
+
+n, m = map(int, input().split())
+arr = [ list(map(int, input().split())) for _ in range(n)]
+answer = 0 # 탈출 할 수 없으면 0, 있으면 1
+que = deque()
+visited = [[False]*m for _ in range(n)]
+dxs, dys = [0, 0,1, -1], [1,-1,0,0]
+    
+
+def in_range(x, y):
+    return 0 <= x < n and 0 <= y < m
+
+def can_go(x, y):
+    if not in_range(x, y):
+        return False
+    if visited[x][y] or arr[x][y] == SNAKE:
+        return False
+    return True
+
+
+def bfs(cur_x, cur_y):
+    global answer
+    que.append((cur_x, cur_y))
+    visited[cur_x][cur_y] = True
+
+    while que:
+        x, y = que.popleft()
+        if (x, y) == (n-1, m-1):
+            answer = 1
+            return 
+        for k in range(4):
+            nx, ny = x+dxs[k], y +dys[k]
+            if can_go(nx, ny):
+                que.append((nx, ny))
+
+bfs(0 ,0)
+print(answer)
