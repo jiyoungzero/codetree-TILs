c69bd3408fb483604d927ee5f3ca01f599a57b2f
Jiyoung Lee <79441145+jiyoungzero@users.noreply.github.com>
2024-01-08 11:01:02 +0900

    Title: 숫자가 가장 큰 인접한 곳으로 동시에 이동, Time: 199ms, Memory: 26MB, Status: Passed - CodeTree

--- 240108/숫자가 가장 큰 인접한 곳으로 동시에 이동/move-to-max-adjacent-cell-simultaneously.py
+++ 240108/숫자가 가장 큰 인접한 곳으로 동시에 이동/move-to-max-adjacent-cell-simultaneously.py
@@ -1,5 +1,6 @@
 import sys
 input = sys.stdin.readline
+from collections import defaultdict
 
 
 dxs, dys = [-1,1,0,0],[0,0,-1,1]
@@ -7,10 +8,10 @@ dxs, dys = [-1,1,0,0],[0,0,-1,1]
 n, m, t = map(int, input().split())
 arr = [list(map(int, input().split())) for _ in range(n)]
 cnt_arr = [[0]*n for _ in range(n)]
-pos = []
+pos = defaultdict(int)
 for _ in range(m):
     r, c = map(int, input().split())
-    pos.append((r-1, c-1))
+    pos[(r-1, c-1)] += 1
 
 def in_range(x, y):
     if x < 0 or y < 0 or n <= x or n <= y:
@@ -20,22 +21,22 @@ def in_range(x, y):
 
 def move(nxtcnt_arr):
     global cnt_arr, pos
-    nxt_pos = []
-    for r, c in pos:
-        fr, fc = -1, -1
+    nxt_pos = defaultdict(int)
+    for (r, c), nums in pos.items():
+        fr, fc = int(1e9), int(1e9)
         for k in range(4):
             nr, nc = r+dxs[k], c+dys[k]
             if not in_range(nr, nc):continue
 
             if in_range(nr, nc):
-                if (fr, fc) == (-1, -1):
+                if (fr, fc) == (int(1e9), int(1e9)):
                     fr, fc = nr, nc
                 elif arr[fr][fc] < arr[nr][nc]:
                     fr, fc = nr, nc
                 else:
                     continue
         nxtcnt_arr[fr][fc] += 1
-        nxt_pos.append((fr, fc))
+        nxt_pos[(fr, fc)] += 1
 
     cnt_arr = [row[:] for row in nxtcnt_arr]
     pos = nxt_pos
@@ -44,10 +45,12 @@ def move(nxtcnt_arr):
 
 def find_collision():
     global cnt_arr
+    global pos
     for i in range(n):
         for j in range(n):
             if cnt_arr[i][j] > 1:
                 cnt_arr[i][j] = 0
+                del pos[(i, j)]
              
 
 
@@ -59,6 +62,18 @@ for _ in range(t):
 
     # cnt_arr에서 1이상인 곳 0으로 만들기
     find_collision()
+
+    # for i in range(n):
+    #     print(nxtcnt_arr[i])
+    # print()
+
+    # print("충돌 후")
+    
+    # for i in range(n):
+    #     print(cnt_arr[i])
+    # print()
+    # print(pos)
+
 
 
 answer = 0
