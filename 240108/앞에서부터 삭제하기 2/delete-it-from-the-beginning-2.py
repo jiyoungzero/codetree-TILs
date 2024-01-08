1c0d672899a5d5a39787db5714f46585742307e9
Jiyoung Lee <79441145+jiyoungzero@users.noreply.github.com>
2024-01-08 15:27:23 +0900

    Title: 앞에서부터 삭제하기 2, Time: 165ms, Memory: 31MB, Status: Passed - CodeTree

--- 240108/앞에서부터 삭제하기 2/delete-it-from-the-beginning-2.py
+++ 240108/앞에서부터 삭제하기 2/delete-it-from-the-beginning-2.py
@@ -3,14 +3,19 @@ input =sys.stdin.readline
 
 n = int(input())
 arr = list(map(int, input().split()))
+heap = []
+heapq.heappush(heap, arr[-1])
 answer = 0
+prefix_sum = heap[0]
+prefix_cnt = 0
 
-for k in range(1, n-1):
-    heap = arr[k:]
-    heapq.heapify(heap)
-    
-    heapq.heappop(heap)
-    answer = max(answer, sum(heap)/len(heap))
+for i in range(n- 2, 0, -1):
+    heapq.heappush(heap, arr[i])
+    prefix_sum += arr[i]
+    prefix_cnt += 1
+
+    min_val = heap[0]
+    answer = max(answer, (prefix_sum-min_val)/(prefix_cnt))
 
 
 print(f"{answer:.2f}")
