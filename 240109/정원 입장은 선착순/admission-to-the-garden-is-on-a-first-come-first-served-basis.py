fbcf45cf36727930d06cc3b0b49ac2d6950ac924
Jiyoung Lee <79441145+jiyoungzero@users.noreply.github.com>
2024-01-09 15:34:23 +0900

    Title: 정원 입장은 선착순, Time: 139ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240109/정원 입장은 선착순/admission-to-the-garden-is-on-a-first-come-first-served-basis.py
+++ 240109/정원 입장은 선착순/admission-to-the-garden-is-on-a-first-come-first-served-basis.py
@@ -1,51 +1,24 @@
 import sys, heapq
 input = sys.stdin.readline
 
-
 n = int(input())
-people = []
 answer = 0
-for i in range(n):
-    a, t = map(int, input().split())
-    people.append((i, a, a+t))
-people.sort(key=lambda x : x[1])
 waiting = []
-visited = [False]*n
-
-enter = people[0]
-idx, in_, out_ = enter
-visited[idx] = True
-key = False
-
+people = []
 for i in range(n):
-    if visited[people[i][0]]:continue
-    if (not visited[people[i][0]] and key):
-        nnidx, nnin_, nnout_ = people[i]
-        answer = max(answer, out_-nnin_)
-        idx, in_, out_ = nnidx, nnin_, nnout_ 
-        visited[idx] = True
-        print("dddd")
+    a, t = tuple(map(int, input().split()))
+    people.append((a, i+1, t))
+people.sort()
+exit_time = 0
 
-    if not key:
-        # waiting걸기
-        for j in range(n):
-            if not visited[people[j][0]]:
-                if people[j] in waiting:continue
-                if people[j][1] < out_:
-                    heapq.heappush(waiting, people[j])
-            
-    if waiting:
-        print(waiting)
-        nxt = heapq.heappop(waiting)
+for a, num, t in people:
+    while exit_time <= a and waiting:
+            (_, na, nt) = heapq.heappop(waiting)
+            answer  = max(answer, exit_time-na)
+            exit_time += nt
 
-        nidx, nin_, nout_ = nxt
-        answer = max(answer, out_-nin_)
-        idx, in_, out_ = nidx, nin_, nout_
-        visited[idx] = True
-        print(visited, idx, nidx)
-
-    if not waiting:
-        key = True
-    print(visited)
-
+    if a > exit_time:
+        exit_time += (a+t)
+    else:
+        heapq.heappush(waiting, (num, a, t))
 print(answer)
