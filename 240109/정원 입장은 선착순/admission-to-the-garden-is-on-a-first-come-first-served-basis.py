a441671e8477397cd45119f36a329e6b394081ee
Jiyoung Lee <79441145+jiyoungzero@users.noreply.github.com>
2024-01-09 15:39:49 +0900

    Title: 정원 입장은 선착순, Time: 123ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240109/정원 입장은 선착순/admission-to-the-garden-is-on-a-first-come-first-served-basis.py
+++ 240109/정원 입장은 선착순/admission-to-the-garden-is-on-a-first-come-first-served-basis.py
@@ -10,15 +10,19 @@ for i in range(n):
     people.append((a, i+1, t))
 people.sort()
 exit_time = 0
+people.append((int(1e9), int(1e9), 0))
 
 for a, num, t in people:
+    # 웨이팅도 있고, 이제 내가 들어갈 수 있을 때,
     while exit_time <= a and waiting:
             (_, na, nt) = heapq.heappop(waiting)
             answer  = max(answer, exit_time-na)
             exit_time += nt
 
+    # 웨이팅도 없고 내가 들어갈 수 있을때
     if a > exit_time:
         exit_time += (a+t)
+    # 웨이팅은 없으나 내가 들어갈 수 없을 때
     else:
         heapq.heappush(waiting, (num, a, t))
 print(answer)
