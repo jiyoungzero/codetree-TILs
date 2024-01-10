20324ecfa7cc9b103d3849e7de7737117a016207
Jiyoung Lee <79441145+jiyoungzero@users.noreply.github.com>
2024-01-10 11:10:30 +0900

    Title: 최소 에너지 비용, Time: 109ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240110/최소 에너지 비용/minimum-energy-cost.py
@@ -0,0 +1,25 @@
+import sys
+input = sys.stdin.readline
+
+n = int(input()) # 장소의 개수
+energy = list(map(int, input().split())) # 장소 간 이동시 드는 에너지
+cost = list(map(int, input().split())) # 에너지1을 채우는데 드는 비용
+
+m = len(energy)
+dp = [0]*(m)
+dp[-1] = energy[-1] 
+for i in range(m-2, -1, -1):
+    dp[i] = dp[i+1] + energy[i]
+dp = dp + [0]
+
+answer = 0
+# 다음 장소의 cost가 현재 cost보다 작다면 충전
+# 아니라면 충전안함
+# 대신 첫 위치에서 cost가 다음 cost보다 크더라도 충전
+for i in range(n-1):
+    print(dp)
+    plus = (dp[i]-dp[i+1])
+    answer += (cost[i]*plus)
+    if plus >= dp[i+1]:
+        break
+print(answer)
