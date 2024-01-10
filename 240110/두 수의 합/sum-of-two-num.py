43c700737a15a1b69ee39ef29f0c47f532fdd48e
Jiyoung Lee <79441145+jiyoungzero@users.noreply.github.com>
2024-01-10 23:18:26 +0900

    Title: 두 수의 합, Time: 223ms, Memory: 41MB, Status: Passed - CodeTree

--- 240110/두 수의 합/sum-of-two-num.py
+++ 240110/두 수의 합/sum-of-two-num.py
@@ -5,15 +5,23 @@ from collections import defaultdict
 n, k = map(int, input().split())
 arr = list(map(int, input().split()))
 num_info = defaultdict(int)
+visited = dict()
 answer = 0
 
 for ele in arr:
     num_info[ele] += 1
+    visited[ele] = False
+
 
 for key, value in num_info.items():
-    if k-key in num_info.keys():
+
+    if (k-key) in num_info.keys() and not visited[k-key]:
+        visited[k-key] = True
+        visited[key] = True
         if key == (k-key):
-            answer += (value*(value-1)/2)
+            answer += (value*(value-1)//2)
+            
         else:
             answer += (value*num_info[k-key])
-print(answer//2)
+        
+print(answer)
