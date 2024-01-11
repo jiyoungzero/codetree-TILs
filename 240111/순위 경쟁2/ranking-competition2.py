e3b2e73f0db0bc61f7160396d48b8d07c4700ba6
Jiyoung Lee <79441145+jiyoungzero@users.noreply.github.com>
2024-01-11 15:11:45 +0900

    Title: 순위 경쟁2, Time: 146ms, Memory: 73MB, Status: Passed - CodeTree

--- 240111/순위 경쟁2/ranking-competition2.py
+++ 240111/순위 경쟁2/ranking-competition2.py
@@ -2,29 +2,27 @@ import sys
 input = sys.stdin.readline
 
 n = int(input())
+changes = [tuple(map(str, input().split())) for _ in range(n)]
 a, b = 0, 0
 answer = 0
 
-winner = 'ab'
-for _ in range(n):
-    who, change = input().split()
-    change = int(change)
-    tmp = ''
-    if who == 'A':
-        a += change
+def get_status(aa, bb):
+    if aa == bb:
+        return 0
+    elif aa > bb:
+        return 1
     else:
-        b += change
+        return 2
 
-    if a == b:
-        tmp = 'ab'
-    elif a > b:
-        tmp = 'a'
+for name, value in changes:
+    value = int(value)
+    if name == 'A':
+        if get_status(a, b) != get_status(a+value, b):
+            answer += 1
+        
+        a += value
     else:
-        tmp = 'b'
-    
-    if winner != tmp:
-        answer += 1
-        winner = tmp
-    else:
-        continue
+        if get_status(a, b) != get_status(a, b+value):
+            answer += 1
+        b += value
 print(answer)
