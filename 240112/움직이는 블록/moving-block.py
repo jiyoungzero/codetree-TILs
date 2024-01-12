478ed414131c31c974b598fd0fc1ce085c67e4e5
Jiyoung Lee <79441145+jiyoungzero@users.noreply.github.com>
2024-01-12 17:38:11 +0900

    Title: 움직이는 블록, Time: 154ms, Memory: 25MB, Status: Passed - CodeTree

--- 240112/움직이는 블록/moving-block.py
+++ 240112/움직이는 블록/moving-block.py
@@ -4,14 +4,9 @@ input = sys.stdin.readline
 n = int(input())
 arr = [int(input()) for _ in range(n)]
 answer = 0
-target = sum(arr)//n
+avg = sum(arr)//n
 
-while len(set(arr)) != 1:
-    arr.sort()
-    tmp = abs(target - arr[0])
-
-    answer += tmp
-
-    arr[-1] -= abs(target-arr[-1])
-    arr[0] += tmp
+for ele in arr:
+    if avg > ele:
+        answer += (avg-ele)
 print(answer)
