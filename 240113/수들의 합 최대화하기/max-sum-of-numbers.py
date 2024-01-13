388da8eeca01ee71776744d3613bf0c4eaaf7ad8
Jiyoung Lee <79441145+jiyoungzero@users.noreply.github.com>
2024-01-13 18:24:04 +0900

    Title: 수들의 합 최대화하기, Time: 110ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240113/수들의 합 최대화하기/max-sum-of-numbers.py
@@ -0,0 +1,33 @@
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+arr = [list(map(int, input().split())) for _ in range(n)]
+answer = 0
+visited = [False]*n
+result = []
+
+def choose(selected):
+    global answer
+    if len(selected) == n:
+        tmp = 0
+        for i, j in zip(selected, [0,1,2]):
+            tmp += arr[i][j]
+
+        answer = max(answer, tmp)
+        return 
+
+    for i in range(n):
+        if visited[i]:
+            continue
+        selected.append(i)
+        visited[i] = True
+        choose(selected)
+        
+        visited[i] = False
+        selected.pop()
+
+
+
+choose([])
+print(answer)
