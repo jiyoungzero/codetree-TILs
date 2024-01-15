b2e6abbfd78457a63e1b087b8994b97fa5b6e76b
Jiyoung Lee <79441145+jiyoungzero@users.noreply.github.com>
2024-01-15 16:30:33 +0900

    Title: 1차원 폭발 게임, Time: 169ms, Memory: 24MB, Status: Passed - CodeTree

--- 240115/1차원 폭발 게임/The-1D-bomb-game.py
+++ 240115/1차원 폭발 게임/The-1D-bomb-game.py
@@ -3,45 +3,27 @@ input = sys.stdin.readline
 
 n, m = map(int, input().split())
 arr = [int(input()) for _ in range(n)]
-bomb_flag = True
 
-def remove(start, cnt):
-    global arr
-    next_arr = []
-    for i in range(start, start+cnt):
-        arr[i] = 0
-    for ele in arr:
-        if ele:
-            next_arr.append(ele)
-    return next_arr
-
-def consecutive(arr):
-    flag = False
-    start, cnt = 0, 1
-    N = len(arr)
-    for i in range(N-1):
-        if arr[i] != arr[i+1] and cnt >= m:
-            start = i-cnt+1
-            flag = True
-            break
-        if arr[i] == arr[i+1]:
-            cnt += 1
-        if i+1 == N-1 and cnt >= m:
-            start = i
-            flag = True
-            break
-    
-        
-    return (flag, start, cnt)
+def get_end_bomb_idx(cur_idx, number):
+    for end_idx in range(cur_idx+1, len(arr)):
+        if arr[end_idx] != number:
+            return end_idx-1
+    return len(arr)-1
 
 
-while 1:
-    N = len(arr)
-    bomb_flag, start, cnt = consecutive(arr)
+while True:
+    bomb_flag= False
+    for cur_idx, number in enumerate(arr):
+        if number == 0: 
+            continue
+
+        end_idx = get_end_bomb_idx(cur_idx, number)
+        if end_idx - cur_idx + 1 >= m:
+            arr[cur_idx:end_idx+1] = [0]*(end_idx-cur_idx+1)
+            bomb_flag = True
+
+    arr = list(filter(lambda x:x>0, arr))
     if not bomb_flag:break
-
-    next_arr = remove(start, cnt)
-    arr = next_arr[:]
 
 print(len(arr))
 for ele in arr:
