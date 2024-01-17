a8d4570ddc360fc018f09e21902001dd218fc65b
Jiyoung Lee <79441145+jiyoungzero@users.noreply.github.com>
2024-01-17 12:48:18 +0900

    Title: 최단 Run Length 인코딩, Time: 121ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240117/최단 Run Length 인코딩/shortest-run-length-encoding.py
@@ -0,0 +1,30 @@
+import sys
+input = sys.stdin.readline
+
+
+given = list(input().rstrip())
+n = len(given)
+answer = int(1e9)
+
+def run_length_encoding(arr):
+    m = len(arr)
+    result = 0
+    idx, nxt_idx = 0, 1
+    while 1: 
+        if idx >= m:
+            break       
+        while nxt_idx < m and arr[nxt_idx] == arr[idx]:
+            idx += 1
+            nxt_idx = idx + 1
+
+        result += 2
+        idx = nxt_idx
+        nxt_idx = idx + 1
+        
+    return result
+
+for _ in range(n):
+    answer = min(answer, run_length_encoding(given))
+    given.insert(0, given.pop())
+
+print(answer)
