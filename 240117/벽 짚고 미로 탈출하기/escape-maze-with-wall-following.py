3302608f2fe37216fbb32e18e1fd04ee9ed31602
Jiyoung Lee <79441145+jiyoungzero@users.noreply.github.com>
2024-01-17 11:50:18 +0900

    Title: 벽 짚고 미로 탈출하기, Time: 102ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240117/벽 짚고 미로 탈출하기/escape-maze-with-wall-following.py
+++ 240117/벽 짚고 미로 탈출하기/escape-maze-with-wall-following.py
@@ -1,52 +1,63 @@
 import sys
-from collections import deque
-input = sys.stdin.readline 
+input =sys.stdin.readline
 
+
+DIR_NUM = 4
 WALL = '#'
 BLANK = '.'
-
-dxs, dys = [0, 1, 0, -1], [1, 0, -1, 0] # R - D - L - U
 n = int(input())
-start_x, start_y = map(int, input().split())
-arr = [input().rstrip() for _ in range(n)]
-start_x -= 1
-start_y -= 1
-check_dir = 1 # D
-cur_dir = 0 # R
-answer = 1
+cur_x, cur_y = tuple(map(int, input().split()))
+arr = [[0]*(n+1) for _ in range(n+1)] 
+for i in range(1, n+1):
+    given_row = input().rstrip()
+    for j, ele in enumerate(given_row):
+        arr[i][j+1] = ele
+
+visited = [[[False]*DIR_NUM for _ in range(n+1)] for _ in range(n+1)] # 탈출 가능 여부
+answer = 0
+dxs, dys = [0, 1, 0, -1], [1, 0, -1, 0] # 시계방향 
+cur_dir = 0
 
 def in_range(x, y):
-    return 0 <= x < n and 0<= y < n
+    return 1 <= x < n and 1<= y < n  
 
-def simulate(sx, sy):
-    global check_dir, cur_dir, answer
-    que = deque()
-    que.append((sx, sy))
-    if arr[sx+1][sy] == BLANK:return -1
-    while que:
-        x, y = que.popleft()
-        nx, ny = x+dxs[cur_dir], y+dys[cur_dir]
-        dx, dy = x+dxs[check_dir], y+dys[check_dir] # 벽짚은 곳
+def wall_exist(x, y):
+    return in_range(x, y) and arr[x][y] == WALL
 
-        if (arr[dx][dy] == WALL and not in_range(nx, ny)): # 탈출
-            return answer
+def simulate():
+    global cur_x, cur_y, cur_dir, answer
+    if visited[cur_x][cur_y][cur_dir]:
+        print(-1)
+        sys.exit(0)
+    visited[cur_x][cur_y][cur_dir] = True
 
+    nx, ny = cur_x+dxs[cur_dir], cur_y + dys[cur_dir]
+    
+    # case1 : 바라보는 방향으로 전진이 불가능한 경우
+    if wall_exist(nx, ny):
+        cur_dir = (cur_dir+3)%4
+    # case2 : 바라보는 방향으로 전진이 가능한 경우 + 탈출
+    elif not in_range(nx, ny):
+        cur_x, cur_y = nx, ny # 탈출
+        answer += 1 
 
-        if arr[dx][dy] == WALL and arr[nx][ny] == WALL:# 반시계 전환 
-            check_dir = (check_dir+3)%4
-            cur_dir = (cur_dir+3)%4
-            que.append((x, y))
-        elif arr[dx][dy] == BLANK: # 시계 전환
-            check_dir = (check_dir+1)%4
+    # case3: 바라보는 방향으로 전진이 가능한 경우
+    else:
+        check_dir = (cur_dir+1)%4
+        # 앞으로 한 칸 전진 후 벽 짚은 곳
+        check_x, check_y = nx + dxs[check_dir], ny+dys[check_dir]
+        # 아래에 벽에 있고 전진이 가능한 경우
+        if wall_exist(check_x, check_y):
+            answer += 1
+            cur_x, cur_y = nx, ny
+
+        # 아래에 벽이 없고 시계방향 회전이 필요한 경우 :  두칸 이동 후 방향 회전
+        else:
+            cur_x, cur_y = check_x, check_y
             cur_dir = (cur_dir+1)%4
-            que.append((x+dxs[cur_dir], y+dys[cur_dir]))
-            answer += 1
+            answer += 2
+    
 
-
-        elif arr[dx][dy] == WALL and arr[nx][ny] == BLANK: # 직진
-            que.append((nx, ny))
-            answer += 1
-    return -1
-
-
-print(simulate(start_x, start_y))
+while in_range(cur_x, cur_y): # 격자 빠져나오기 전까지 반복
+    simulate()
+print(answer)
