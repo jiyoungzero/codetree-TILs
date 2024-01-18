9b22b7d127f38d13199252b6fdbe810d61f7c9d0
Jiyoung Lee <79441145+jiyoungzero@users.noreply.github.com>
2024-01-18 23:36:20 +0900

    Title: 쪼개어 배낭 채우기 구현, Time: 375ms, Memory: 27MB, Status: Wrong Answer - CodeTree

---
+++ 240118/쪼개어 배낭 채우기 구현/implement-fractional-knapsack.py
@@ -0,0 +1,31 @@
+import sys
+input =sys.stdin.readline
+
+n , m = map(int ,input().split())
+jewelrys = [tuple(map(int, input().split())) for _ in range(n)]
+info = []
+# 무게 당 높은 가격 순으로 정렬,  round(answer, 3)
+for idx, jewelry in enumerate(jewelrys):
+    w, v = jewelry
+    unit_price = v/w
+
+    info.append((round(unit_price, 4), idx))
+info.sort(reverse=True)
+
+bag = 0
+answer = 0
+
+for target in info:
+    if bag >= m:break
+    unit_price, idx = target
+
+    if jewelrys[idx][0] + bag <= m:
+        bag += jewelrys[idx][0]
+        answer += jewelrys[idx][1]
+    else: # 쪼개기
+        possible = m - bag
+        answer += possible*unit_price
+        break
+
+
+print(format(round(answer, 3), '.3f'))
