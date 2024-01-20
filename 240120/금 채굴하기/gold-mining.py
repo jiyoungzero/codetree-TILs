e3fe6fdb6a10147a986a928db21aec91412be66b
Jiyoung Lee <79441145+jiyoungzero@users.noreply.github.com>
2024-01-20 12:24:43 +0900

    Title: 금 채굴하기, Time: 146ms, Memory: 26MB, Status: Wrong Answer - CodeTree

--- 240120/금 채굴하기/gold-mining.py
+++ 240120/금 채굴하기/gold-mining.py
@@ -1,61 +1,43 @@
+# abs(nx-x)+abs(ny-y) <= k
+
 import sys
-input = sys.stdin.readline
-from collections import deque
+input = sys.stdin.readline 
 
 GOLD = 1
-BLANK = 0
-
 n, m = map(int, input().split())
 arr = [list(map(int, input().split())) for _ in range(n)]
-dxs, dys = [0,0,1,-1], [1,-1,0,0]
 answer = 0
 
-
 def in_range(x, y):
-    return 0 <= x <n and 0 <= y < n
+    return 0 <= x < n and 0 <= y <= n
 
-def get_gold_for_k(k, x, y):
-    gold_value = 0
-    que = deque()
-    que.append((x, y))
-    visited = [[False]*n for _ in range(n)]
-    visited[x][y] = True
-    if arr[x][y] == GOLD:
-        gold_value += m 
-    
-    # k == 0인 경우
-    if k == 0:return gold_value
-        
-    time = get_findcost(k-1)
-    for _ in range(time):
-        cur_x, cur_y = que.popleft()
+def in_square(nx, ny, x, y, k):
+    if abs(nx-x)+abs(ny-y) <= k:
+        return True
+    else:
+        return False
 
-        for d in range(4):
-            nx, ny = cur_x+dxs[d], cur_y+dys[d]
-            if in_range(nx, ny) and not visited[nx][ny]:
-                if arr[nx][ny] == GOLD:
-                    gold_value += m 
-                visited[nx][ny] = True
-            que.append((nx, ny))
-    # if k > 2:
-    #     print("k=", k)
-    #     for row in visited:
-    #         print(*row)
- 
-    return gold_value
-        
-
-
-def get_findcost(k):
+def get_find_cost(k):
     return k*k+(k+1)*(k+1)
+
+def get_gold_value(k, i, j):
+    visited = [[False]*n for _ in range(n)]
+    result = 0
+    for row in range(n):
+        for col in range(n):
+            if in_square(row, col, i, j, k):
+                if not visited[row][col]:
+                    visited[row][col] = True
+                    if arr[row][col] == GOLD:
+                        result += m 
+    return result
+
 
 for i in range(n):
     for j in range(n):
-        for size in range(n+2):
-            gold_value = get_gold_for_k(size, i, j)
-            gold_cnt = gold_value//m
-            if answer < gold_cnt and get_findcost(size) <= gold_value:
-                answer = gold_cnt
-                
+        for size in range(n):
+            gold_value = get_gold_value(size, i, j) 
+            if gold_value >= get_find_cost(size):
+                answer = max(answer, gold_value//m)
 
 print(answer)
