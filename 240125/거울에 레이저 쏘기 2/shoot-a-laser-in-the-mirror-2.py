a1061e21c7affdb06d9862e8b39cf389034d8b3b
Jiyoung Lee <79441145+jiyoungzero@users.noreply.github.com>
2024-01-25 13:55:10 +0900

    Title: 거울에 레이저 쏘기 2, Time: 206ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240125/거울에 레이저 쏘기 2/shoot-a-laser-in-the-mirror-2.py
+++ 240125/거울에 레이저 쏘기 2/shoot-a-laser-in-the-mirror-2.py
@@ -1,59 +1,44 @@
 import sys
-input = sys.stdin.readline 
-
+input = sys.stdin.readline
 
 n = int(input())
-arr = []
-for _ in range(n):
-    command = input().rstrip()
-    tmp = []
-    flag = False
-    for ele in command:
-        if ele == '/':
-            tmp.append(ele)
-            continue
-        if not flag:
-            tmp.append('\\')
-            flag = True
-        if flag:
-            flag = False
-    arr.append(tmp)
-
-            
-start = int(input()) 
-x, y = start//n, start%n-1
-cur_dir = 0
-up_slash = {0:3, 3:0, 2:1, 1:2}
-down_slash = {0:1, 1:0, 2:3, 3:2}
-if arr[x][y] == '\\': # 0: 밑으로, 1 : 오른쪽으로, 2: 위쪽으로, 3 : 왼쪽으로 
-    cur_dir = down_slash[x]
+arr = [ list(input()) for _ in range(n)]
+k = int(input())
+dxs, dys = [1, 0, -1, 0],[0, 1, 0, -1]  # 아래, 오른쪽, 위, 왼쪽
+x, y = (k-1)//n, k%n
+if y == 0:
+    y = n-1
 else:
-    cur_dir = up_slash[x]
-
-dxs, dys = [1,0,-1,0], [0, 1,0,-1]
+    y -= 1
+cur_dir = x
 answer = 1
-
 
 def in_range(x, y):
     return 0 <= x < n and 0 <= y < n
 
 while True:
-    # print(x, y, cur_dir)
-    if arr[x][y] == '\\': # down_slash
-        nx, ny = x + dxs[cur_dir], y + dys[cur_dir]
-        if not in_range(nx, ny):
-            break
-        x, y = nx, ny
-        # 방향 바꾸기
-        nxt_dir = down_slash[cur_dir]
-        cur_dir = nxt_dir
-    else: # up_slash
-        nx, ny = x + dxs[nxt_dir], y + dys[nxt_dir]
-        if not in_range(nx, ny):
-            break
-        x, y = nx, ny
-        # 한번 더 방향 바꾸기
-        nxt_dir = up_slash[nxt_dir]
-        cur_dir = nxt_dir
+    if arr[x][y] == '/':
+        if cur_dir == 0:
+            cur_dir = 3 #
+        elif cur_dir == 3:
+            cur_dir = 0
+        elif cur_dir == 1:
+            cur_dir = 2
+        else:
+            cur_dir = 1
+    else:#dxs, dys = [0, 1, -1, 0],[1,0,0,-1] # 아래, 오른쪽, 위, 왼쪽
+        if cur_dir == 0 : 
+            cur_dir = 1 
+        elif cur_dir == 1:
+            cur_dir = 0
+        elif cur_dir == 2:
+            cur_dir = 3
+        else:
+            cur_dir = 2
+    nx, ny = x + dxs[cur_dir], y + dys[cur_dir]
+
+    if not in_range(nx, ny):
+        break
+    x, y = nx, ny
     answer += 1
 print(answer)
