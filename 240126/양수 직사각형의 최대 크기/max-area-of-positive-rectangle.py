9cf6033e4588bbe0a27e27e21bd28e737115b6b9
Jiyoung Lee <79441145+jiyoungzero@users.noreply.github.com>
2024-01-26 21:11:21 +0900

    Title: 양수 직사각형의 최대 크기, Time: 137ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240126/양수 직사각형의 최대 크기/max-area-of-positive-rectangle.py
+++ 240126/양수 직사각형의 최대 크기/max-area-of-positive-rectangle.py
@@ -5,6 +5,8 @@ n, m = map(int, input().split())
 arr = [list(map(int, input().split())) for _ in range(n)]
 answer = 0
 
+
+
 def all_plus(si, sj, ei, ej):
     for i in range(si, ei+1):
         for j in range(sj, ej+1):
@@ -14,8 +16,9 @@ def all_plus(si, sj, ei, ej):
 
 for si in range(n):
     for sj in range(m):
-        for ei in range(si+1, n):
-            for ej in range(sj+1, m):
+        for ei in range(si, n):
+            for ej in range(sj, m):
                 if all_plus(si, sj, ei, ej):
                     answer = max(answer, (ei-si+1)*(ej-sj+1))
+    
 print(answer)
