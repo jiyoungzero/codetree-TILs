bee2e38ead02fbc18da43b53994f2bd039066aaf
Jiyoung Lee <79441145+jiyoungzero@users.noreply.github.com>
2024-01-27 12:15:51 +0900

    Title: 기울어진 직사각형의 회전, Time: 149ms, Memory: 25MB, Status: Passed - CodeTree

--- 240127/기울어진 직사각형의 회전/rotate-slanted-rectangle.py
+++ 240127/기울어진 직사각형의 회전/rotate-slanted-rectangle.py
@@ -1,53 +1,43 @@
-import sys
-input = sys.stdin.readline 
-from collections import deque
+CCW = 0 # 반시계
+CW = 1 # 시계
 
+# 변수 선언 및 입력:
 n = int(input())
-arr = [list(map(int, input().split())) for _ in range(n)]
-r, c, m1, m2, m3, m4, r_dir = map(int, input().split())
-# r_dir == 0 : 반시계 방향 
-# r_dir == 1 : 시계 방향
-m_dir = [(-1, 1), (-1, -1), (1, -1), (1, 1)]
-m_lst = [m1, m2, m3, m4-1]
-r -= 1
-c -= 1
+grid = [
+    list(map(int, input().split()))
+    for _ in range(n)
+]
+temp = [
+    [0 for _ in range(n)]
+    for _ in range(n)
+]
 
-def get_que():
-    copy_r, copy_c = r, c
-    que = deque()
-    que.append(arr[r][c])
-
-    for k in range(4):
-        for _ in range(m_lst[k]):
-            nr, nc = copy_r + m_dir[k][0], copy_c+m_dir[k][1]
-            que.append(arr[nr][nc])
-            copy_r, copy_c = nr, nc
-    return que
-
-def rotate(r_dir):
-    q = get_que()
-    if r_dir == 0: 
-        tmp = q.pop()
-        q.appendleft(tmp)
+def shift(x, y,k, l, move_dir):
+    if move_dir == CCW: # 반시계
+        dxs, dys = [-1, -1, 1, 1], [1, -1, -1, 1]
+        move_nums = [k, l, k, l]
     else:
-        tmp = q.popleft()
-        q.append(tmp)
-    return q 
+        dxs, dys = [-1, -1, 1, 1], [-1, 1, 1, -1]
+        move_nums = [l, k, l, k]
+    for i in range(n):
+        for j in range(n):
+            temp[i][j] = grid[i][j]
+    
+    for dx, dy, move_num in zip(dxs, dys, move_nums):
+        for _ in range(move_num):
+            nx, ny = x + dx, y + dy
+            temp[nx][ny] = grid[x][y]
+            x, y = nx, ny
+    
+    for i in range(n):
+        for j in range(n):
+            grid[i][j] = temp[i][j]
 
-def rewrite():
-    global arr, r, c
-    copy_r, copy_c = r, c
-    new_q = rotate(r_dir)
-    arr[r][c] =  new_q.popleft()  
-    for k in range(4):
-        for _ in range(m_lst[k]):
-            nr, nc = copy_r + m_dir[k][0], copy_c+m_dir[k][1]
-            arr[nr][nc] = new_q.popleft()
-            copy_r, copy_c = nr, nc
-    return 
 
-rewrite()
+x, y, m1, m2, m3, m4, d = tuple(map(int, input().split()))
+shift(x - 1, y - 1, m1, m2, d)
 
-# 정답 출력
-for row in arr:
-    print(*row)
+for i in range(n):
+    for j in range(n):
+        print(grid[i][j], end=" ")
+    print()
