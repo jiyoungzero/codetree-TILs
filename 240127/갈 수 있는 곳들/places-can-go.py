afa6a12f01b0c35d13a4d3e11d060365e937d6f8
Jiyoung Lee <79441145+jiyoungzero@users.noreply.github.com>
2024-01-27 13:41:55 +0900

    Title: 갈 수 있는 곳들, Time: 173ms, Memory: 25MB, Status: Wrong Answer - CodeTree

---
+++ 240127/갈 수 있는 곳들/places-can-go.py
@@ -0,0 +1,31 @@
+from collections import deque
+WALL = 1
+
+n, k = map(int, input().split())
+arr = [list(map(int, input().split())) for _ in range(n)]
+start_spots = deque()
+dxs, dys = [0,0,1,-1],[1,-1,0,0]
+visited = [[False]*n for _ in range(n)]
+answer = k
+
+for _ in range(k):
+    r, c = map(int, input().split())
+    start_spots.append((r-1, c-1))
+    visited[r-1][c-1] = True
+
+def in_range(x, y):
+    return 0<=x<n and 0<= y<n
+
+while start_spots:
+    x, y = start_spots.popleft()
+    for kk in range(4):
+        nx, ny = x + dxs[kk], y + dys[kk]
+        if not in_range(nx,ny):
+            continue
+        if not visited[nx][ny] and arr[nx][ny] != WALL:
+            visited[nx][ny] = True
+            start_spots.append((nx,ny))
+            x, y = nx, ny
+            answer += 1
+
+print(answer)
