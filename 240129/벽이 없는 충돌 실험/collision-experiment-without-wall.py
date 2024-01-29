6a0c9685c5898b5ae205ec06dc39a2ea3dc4f7a6
Jiyoung Lee <79441145+jiyoungzero@users.noreply.github.com>
2024-01-29 15:09:41 +0900

    Title: 벽이 없는 충돌 실험, Time: 274ms, Memory: 149MB, Status: Wrong Answer - CodeTree

--- 240129/벽이 없는 충돌 실험/collision-experiment-without-wall.py
+++ 240129/벽이 없는 충돌 실험/collision-experiment-without-wall.py
@@ -1,79 +1,80 @@
+# 충돌
+ # 중간에 충돌하는 것도 반영하기 위하여 x, y위치는 2배로 잡는다.
+# 시간 
 BLANK = -1
+import sys
+input = sys.stdin.readline 
 
-t = int(input())
-n = 0
-marbles = []
-nxt_marbles = []
 dxs, dys = [0, 1, -1, 0], [1, 0, 0, -1]
-nxt_arr = [
-    [BLANK for _ in range(4001)]
-    for _ in range(4001)
-]
-
-cur_time = 0
-last_collision_time = -1
-
 mapper = {
     'U': 0,
     'R': 1,
     'L': 2,
     'D': 3  
 }
+balls = []
+nxt_balls = []
+dup_arr = [[-1]*4001 for _ in range(4001)]
+cur_time = 0
+answer = -1
 
 def in_range(x, y):
     return 0 <= x < 4001 and 0 <= y < 4001
 
-def move(marble):
-    global last_collision_time
-    x, y, w, d, i = marble 
-    nx,ny = x + dxs[d], y+dys[d]
-    return (nx, ny, w, d, i)
+def move(ball):
+    x, y, w, d, idx = ball
+    nx, ny = x +dxs[d], y+dys[d]
+    return (nx, ny, w, d, idx)
 
-def collide(marble1, marble2):
-    if (marble1[2] > marble2[2]) or (marble1[2] == marble2[2] and marble1[4] > marble2[4]):
-        return marble1
+def collide(ball1, ball2):
+    x1, y1, w1, d1, idx1 = ball1
+    x2, y2, w2, d2, idx2 = ball2
+    if w1 > w2 or (w1 == w2 and idx1 > idx2):
+        return ball1
     else:
-        return marble2
+        return ball2
 
-def push_next_marble(marble):
-    global last_collision_time
-    x, y, w, d, i = marble
-    if not in_range(x, y):
-        return 
-    
-    original_marble_idx = nxt_arr[x][y]
-    if original_marble_idx == BLANK:
-        nxt_marbles.append(marble)
-        nxt_arr[x][y] = len(nxt_marbles) - 1
+def push_nxt_balls(nxt_ball):
+    global answer, dup_arr
+    nx, ny, w, d, idx = nxt_ball
+
+    if not in_range(nx, ny):
+        return # 무시
+    original_ball_idx = dup_arr[nx][ny]
+    if original_ball_idx == BLANK:
+        nxt_balls.append((nx, ny, w, d, idx))
+        dup_arr[nx][ny] = len(nxt_balls) - 1
     else:
-        nxt_marbles[original_marble_idx] = collide(nxt_marbles[original_marble_idx], marble)
-        last_collision_time = cur_time
+        target = collide(nxt_balls[original_ball_idx], nxt_ball)
+        tx, ty, tw, td, tidx = target
+        nxt_balls[original_ball_idx] = target
+        dup_arr[tx][ty] = tidx
+        answer = cur_time
+
 
 def simulate():
-    global marbles, nxt_marbles
-    for marble in marbles:
-        nxt_marble = move(marble)
-        push_next_marble(nxt_marble)
+    global balls, nxt_balls
+    for ball in balls:
+        nxt_ball = move(ball)
+        push_nxt_balls(nxt_ball)
+    
+    balls = nxt_balls[:]
+    for x, y, _, _, _ in nxt_balls:
+        dup_arr[x][y] = BLANK
+    nxt_balls = []
 
-    marbles = nxt_marbles[:]
-    for x, y, _, _, _ in nxt_marbles:
-        nxt_arr[x][y] = BLANK
-    nxt_marbles = []
-
+t = int(input())
 for _ in range(t):
-    marbles = []
-    last_collision_time = -1
-
     n = int(input())
-    for i in range(1, n+1):
+    balls = []
+    for idx in range(n):
         x, y, w, d = map(str, input().split())
-        x, y, w = int(x), int(y), int(w)
-        # 중간에 충돌하는 경우를 깔끔하게 하기 위해 
-        x = x * 2
-        y = y * 2
-        marbles.append([x+2000, y+2000, w, mapper[d], i])
-
+        x = int(x)*2 + 2000
+        y = int(y)*2 + 2000
+        w = int(w)
+        balls.append((x, y, w, mapper[d], idx))
+    
     for i in range(1, 4001):
         cur_time = i
         simulate()
-    print(last_collision_time)
+    print(answer)
