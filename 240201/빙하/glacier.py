dd3b0b0a531b16920a89cbb968f054e36a6c4753
Jiyoung Lee <79441145+jiyoungzero@users.noreply.github.com>
2024-02-01 10:42:42 +0900

    Title: 빙하, Time: 1493ms, Memory: 32MB, Status: Passed - CodeTree

--- 240201/빙하/glacier.py
+++ 240201/빙하/glacier.py
@@ -9,6 +9,7 @@ dxs, dys = [0,0,1,-1], [1,-1,0,0]
 n, m = map(int, input().split())
 arr = [list(map(int, input().split())) for _ in range(n)]
 time, last_ice_size = 0, 0
+que = deque()
 
 def in_range(x, y):
     return 0 <= x < n and 0 <= y < m
@@ -23,7 +24,6 @@ def get_ice_size():
     return result
 
 def get_outer_water():
-    que = deque()
     visited = [[False]*m for _ in range(n)]
     que.append((0,0))
     visited[0][0] = True
@@ -44,11 +44,6 @@ def get_outer_water():
 
 def melt_ice(outer_water):
     global arr
-    nxt_arr = [[0]*m for _ in range(n)]
-
-    for i in range(n):
-        for j in range(m):
-            nxt_arr[i][j] = arr[i][j] # 녹은 후의 arr를 위한 복사
 
     for outer_water_pos in outer_water_lst:
         x, y = outer_water_pos
@@ -57,12 +52,7 @@ def melt_ice(outer_water):
             if not in_range(nx, ny):
                 continue
             if arr[nx][ny] == ICE:
-                nxt_arr[nx][ny] = WATER
-    
-    # arr에 nxt_arr 복사
-    for i in range(n):
-        for j in range(m):
-            arr[i][j] = nxt_arr[i][j]
+                arr[nx][ny] = WATER
     return   
 
 def ice_exist():
