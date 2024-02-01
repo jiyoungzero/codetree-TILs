1736a27cbf191384830a768c4f8ce6e9e2da4a3e
Jiyoung Lee <79441145+jiyoungzero@users.noreply.github.com>
2024-02-01 15:33:54 +0900

    Title: 4가지 연산을 이용하여 1 만들기, Time: 145ms, Memory: 25MB, Status: Passed - CodeTree

--- 240201/4가지 연산을 이용하여 1 만들기/make-one-using-four-operations.py
+++ 240201/4가지 연산을 이용하여 1 만들기/make-one-using-four-operations.py
@@ -5,7 +5,7 @@ from collections import deque
 n = int(input())
 que = deque()
 que.append((n, 0))
-visited = []
+visited = [n]
 answer = 0
 
 def bfs():
@@ -15,12 +15,19 @@ def bfs():
         if now_num == 1:
             answer = time
             return 
-        if now_num % 3 == 0:
+
+        if now_num//3 not in visited and now_num % 3 == 0:
+            visited.append(now_num//3)
             que.append((now_num//3, time+1))
-        if now_num % 2 == 0:
+        if now_num//2 not in visited and now_num % 2 == 0:
+            visited.append(now_num//2)
             que.append((now_num//2, time+1))
-        que.append((now_num-1, time+1))
-        que.append((now_num+1, time+1))
+        if now_num-1 not in visited:
+            visited.append(now_num-1)
+            que.append((now_num-1, time+1))
+        if now_num+1 not in visited:
+            visited.append(now_num+1)
+            que.append((now_num+1, time+1))
 
 bfs()
 print(answer)
