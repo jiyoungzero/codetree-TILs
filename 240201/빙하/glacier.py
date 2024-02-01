b37bd19b92bef6bcde305f3e7f7f7ce7ee6a99f6
Jiyoung Lee <79441145+jiyoungzero@users.noreply.github.com>
2024-02-01 10:39:09 +0900

    Title: 빙하, Time: 1847ms, Memory: 32MB, Status: Passed - CodeTree

--- 240201/빙하/glacier.py
+++ 240201/빙하/glacier.py
@@ -24,9 +24,10 @@ def get_ice_size():
 
 def get_outer_water():
     que = deque()
-    outer_water = [[False]*m for _ in range(n)]
+    visited = [[False]*m for _ in range(n)]
     que.append((0,0))
-    outer_water[0][0] = True
+    visited[0][0] = True
+    outer_water_lst = [(0,0)]
 
     while que:
         x, y = que.popleft()
@@ -34,42 +35,44 @@ def get_outer_water():
             nx, ny = x+dxs[k], y+dys[k]
             if not in_range(nx, ny):
                 continue
-            if not outer_water[nx][ny]:
+            if not visited[nx][ny]:
                 if arr[nx][ny] == WATER:
                     que.append((nx, ny))
-                    outer_water[nx][ny] = True
-    return outer_water
+                    visited[nx][ny] = True
+                    outer_water_lst.append((nx, ny))
+    return outer_water_lst
 
 def melt_ice(outer_water):
     global arr
     nxt_arr = [[0]*m for _ in range(n)]
-    visited = [[False]*m for _ in range(n)]
 
-    que = deque()
     for i in range(n):
         for j in range(m):
             nxt_arr[i][j] = arr[i][j] # 녹은 후의 arr를 위한 복사
-            if outer_water[i][j]:
-                que.append((i, j)) # ice를 녹일 수 있는 물
-                visited[i][j] = True
-    while que:
-        x, y = que.popleft()
+
+    for outer_water_pos in outer_water_lst:
+        x, y = outer_water_pos
         for k in range(4):
             nx, ny = x+dxs[k], y+dys[k]
             if not in_range(nx, ny):
                 continue
-            if arr[nx][ny] == ICE and not visited[nx][ny]:
+            if arr[nx][ny] == ICE:
                 nxt_arr[nx][ny] = WATER
-                visited[nx][ny] = True
     
     # arr에 nxt_arr 복사
     for i in range(n):
         for j in range(m):
             arr[i][j] = nxt_arr[i][j]
     return   
+
+def ice_exist():
+    for i in range(n):
+        for j in range(m):
+            if arr[i][j] == ICE:
+                return True
+    return False
     
-    
-while get_ice_size() > 0 :
+while ice_exist():
     time += 1
     outer_water_lst = get_outer_water()
     last_ice_size = get_ice_size()
