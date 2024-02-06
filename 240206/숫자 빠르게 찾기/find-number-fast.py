1439c48132507219c629731e3c8f4773b924f97d
Jiyoung Lee <79441145+jiyoungzero@users.noreply.github.com>
2024-02-06 21:01:43 +0900

    Title: 숫자 빠르게 찾기, Time: 5029ms, Memory: 29MB, Status: Time Limit Exceed - CodeTree

---
+++ 240206/숫자 빠르게 찾기/find-number-fast.py
@@ -0,0 +1,21 @@
+import sys
+input = sys.stdin.readline 
+
+n, m = map(int, input().split())
+arr = list(map(int, input().split()))
+def binary_search(target):
+    global arr
+    left,right = 0, len(arr)-1
+    while left <= right:
+        mid = (left+right)//2
+        if arr[mid] == target:
+            return mid+1
+        if arr[mid] < target:
+            left += 1
+        else:
+            right -= 1
+    return -1
+
+for _ in range(m):
+    target = int(input())
+    print(binary_search(target))
