328e2fe6c72f7f1f9bd6e36dabd23e705c6a53be
Jiyoung Lee <79441145+jiyoungzero@users.noreply.github.com>
2024-02-07 09:19:17 +0900

    Title: 숫자 빠르게 찾기, Time: 252ms, Memory: 31MB, Status: Passed - CodeTree

--- 240207/숫자 빠르게 찾기/find-number-fast.py
+++ 240207/숫자 빠르게 찾기/find-number-fast.py
@@ -12,10 +12,10 @@ def binary_search(target):
         mid = (left+right)//2
         if arr[mid] == target:
             return mid
-        if arr[mid] > target:
-            right = mid - 1
-        else:
+        if arr[mid] < target:
             left = mid + 1
+        else:
+            right = mid - 1
     return -1
 
 
