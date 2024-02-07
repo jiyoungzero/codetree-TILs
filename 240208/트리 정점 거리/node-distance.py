d0e919ac75aa0b814d26923261c90f96eb93a31e
Jiyoung Lee <79441145+jiyoungzero@users.noreply.github.com>
2024-02-08 00:02:44 +0900

    Title: 트리 정점 거리, Time: 177ms, Memory: 26MB, Status: Wrong Answer - CodeTree

---
+++ 240208/트리 정점 거리/node-distance.py
@@ -0,0 +1,28 @@
+import sys
+input = sys.stdin.readline 
+
+n, m = map(int, input().split())
+tree = [[] for _ in range(n+1)]
+answer = 0
+for _ in range(n-1):
+    a, b, cost = map(int, input().split())
+    tree[a].append((b, cost))
+    tree[b].append((a, cost))
+
+def dfs(node, target, dist):
+    global answer
+    if node == target:
+        answer = dist
+        return 
+    for leaf, cost in tree[node]:
+        if not visited[leaf]:
+            visited[leaf] = True
+            dfs(leaf, target, dist+cost)
+
+for _ in range(m):
+    answer = 0
+    visited = [False]*(n+1)
+    a, b = map(int, input().split())
+    
+    dfs(a, b, 0)
+    print(answer)
