6b4a55e5d5f9e01e70ea2dcd17a836cdaeeef5e9
Jiyoung Lee <79441145+jiyoungzero@users.noreply.github.com>
2024-02-14 23:50:41 +0900

    Title: 트리의 지름, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240214/트리의 지름/diameter-of-tree.py
@@ -0,0 +1,35 @@
+import sys
+input = sys.stdin.readline 
+
+n = int(input())
+edges = [[] for _ in range(n+1)]
+visited = [False]*(n+1)
+distance = [0]*(n+1)
+
+for _ in range(n-1):
+    a, b, d = map(int, input().split())
+    edges[a].append((b, d))
+    edges[b].append((a, d))
+
+def dfs(node, total):
+    for leaf, d in edges[node]:
+        if not visited[leaf]:
+            visited[leaf] = True
+            distance[leaf] = total + d
+            dfs(leaf, total+d)
+
+def find_far_vertex(x):
+    # visited = [False]*(n+1)
+    # distance = [0]*(n+1)
+    for i in range(1, n + 1):
+        visited[i] = False
+        distance[i] = 0
+    visited[x] = True
+    dfs(x, 0)
+
+    return distance.index(max(distance)), max(distance)
+
+nxt_vertex, _ = find_far_vertex(1)
+_, answer = find_far_vertex(nxt_vertex)
+
+print(answer)
