013f018f0f4729cc77b1700d7b34c6762d59b067
Jiyoung Lee <79441145+jiyoungzero@users.noreply.github.com>
2024-02-17 23:41:59 +0900

    Title: 호텔 예약, Time: 115ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240217/호텔 예약/reserve-hotel.py
+++ 240217/호텔 예약/reserve-hotel.py
@@ -3,20 +3,16 @@ input = sys.stdin.readline
 
 n = int(input())
 points = []
-answer = 1
+answer = 0
 
 for i in range(n):
     s, e = map(int, input().split())
-    points.append((s, e))
+    points.append((s, 1))
+    points.append((e, -1))
 
 points.sort()
-s, e = points[0]
-
-for point in points[1:]:
-    ns, ne = point
-    if ns >= e:
-        s, e = ns, ne
-    else:
-        answer += 1
-        s, e = ns, ne
+tmp = 0
+for x, v in points:
+    tmp += v
+    answer = max(answer, tmp)
 print(answer)
