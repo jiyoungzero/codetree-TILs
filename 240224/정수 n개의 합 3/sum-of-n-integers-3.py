fc5ef4ac5acf3d2541509fcea513c3e38a5e7cb3
Jiyoung Lee <79441145+jiyoungzero@users.noreply.github.com>
2024-02-24 00:55:45 +0900

    Title: 정수 n개의 합 3, Time: 117ms, Memory: 26MB, Status: Wrong Answer - CodeTree

--- 240224/정수 n개의 합 3/sum-of-n-integers-3.py
+++ 240224/정수 n개의 합 3/sum-of-n-integers-3.py
@@ -14,9 +14,10 @@ for i in range(1, n+1):
         prefix_sum[i][j] = arr[i][j] + prefix_sum[i-1][j] + prefix_sum[i][j-1] - prefix_sum[i-1][j-1]
 
 
+
 answer = 0
-for i in range(k, n+1-k):
-    for j in range(k, n+1-k):
+for i in range(k, n+1):
+    for j in range(k, n+1):
         tmp_sum = prefix_sum[i][j] - prefix_sum[i-k][j] - prefix_sum[i][j-k] + arr[i-k][j-k]
         answer = max(answer, tmp_sum)
 print(answer)
