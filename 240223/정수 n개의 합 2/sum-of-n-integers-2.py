93c141b97b9eb5fc0de20c474aa44982c692087a
Jiyoung Lee <79441145+jiyoungzero@users.noreply.github.com>
2024-02-23 23:35:13 +0900

    Title: 정수 n개의 합 2, Time: 181ms, Memory: 31MB, Status: Passed - CodeTree

---
+++ 240223/정수 n개의 합 2/sum-of-n-integers-2.py
@@ -0,0 +1,17 @@
+import sys
+input = sys.stdin.readline 
+
+
+n, k = map(int, input().split())
+arr = list(map(int, input().split()))
+dp = [0]*n
+dp[0] = arr[0]
+
+
+for i in range(1, n):
+    dp[i] = dp[i-1]+arr[i]
+
+answer = -1
+for i in range(k, n):
+    answer = max(answer, dp[i]-dp[i-k])
+print(answer)
