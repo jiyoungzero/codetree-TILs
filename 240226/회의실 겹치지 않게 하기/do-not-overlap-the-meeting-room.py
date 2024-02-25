bf8720a91f7ad38469d85fde8640bac346800efc
Jiyoung Lee <79441145+jiyoungzero@users.noreply.github.com>
2024-02-26 00:18:12 +0900

    Title: 회의실 겹치지 않게 하기, Time: 427ms, Memory: 33MB, Status: Passed - CodeTree

--- 240226/회의실 겹치지 않게 하기/do-not-overlap-the-meeting-room.py
+++ 240226/회의실 겹치지 않게 하기/do-not-overlap-the-meeting-room.py
@@ -6,18 +6,13 @@ schedule = [list(map(int, input().split())) for _ in range(n)]
 schedule.sort(key = lambda x : x[1])
 answer = 0
 
-ss, ee = schedule[0][0], schedule[0][1]
+last_e, max_cnt = -1, 0
+for s, e in schedule:
+    if s >= last_e:
+        max_cnt += 1
+        last_e = e
 
-for i in range(1, n):
-    s, e = schedule[i]
-    if ee <= s:
-        ss, ee = s, e
-        continue
-    else:
-        answer += 1
-        # ss, ee, = schedule[i-2][0], schedule[i-2][1]
-
-
+answer = n - max_cnt
 print(answer)
 
 # 7
