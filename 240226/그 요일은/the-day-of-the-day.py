36ba2d911c851244a3af474e9c42ef1cb9b019f4
Jiyoung Lee <79441145+jiyoungzero@users.noreply.github.com>
2024-02-26 13:15:44 +0900

    Title: 그 요일은, Time: 143ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240226/그 요일은/the-day-of-the-day.py
+++ 240226/그 요일은/the-day-of-the-day.py
@@ -1,6 +1,7 @@
 m1, d1, m2, d2 = map(int, input().split())
 
 num_of_day = [0, 31, 29, 31,30,31,30,31,31,30,31,30,31]
+A = input()
 week = ['Mon', 'Tue', 'Wed', 'Thu','Fri', 'Sat', 'Sun']
 
 def to_day(m, d):
@@ -12,4 +13,15 @@ def to_day(m, d):
 
 day1 = to_day(m1, d1)
 day2 = to_day(m2, d2)
-print((day2-day1+1)//7+1)
+target = (week.index(A))%7
+now = day1
+
+
+answer = 0
+while now <= day2:
+    if now%7 == target:
+        answer += 1
+        now += 7
+    else:
+        now += 1
+print(answer)
