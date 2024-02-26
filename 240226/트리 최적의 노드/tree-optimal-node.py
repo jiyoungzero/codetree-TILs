fada87664835ffbd6f953e7d5a69fa9c88d25020
Jiyoung Lee <79441145+jiyoungzero@users.noreply.github.com>
2024-02-26 12:22:55 +0900

    Title: 트리 최적의 노드, Time: 5094ms, Memory: 49MB, Status: Time Limit Exceed - CodeTree

---
+++ 240226/트리 최적의 노드/tree-optimal-node.py
@@ -0,0 +1,37 @@
+import sys
+input = sys.stdin.readline 
+
+
+n = int(input())
+farthest_dist =  [[] for _ in range(n+1)]
+edges = [[] for _ in range(n+1)]
+for _ in range(n-1):
+    a, b = map(int, input().split())
+    edges[a].append((b, 1))
+    edges[b].append((a, 1))
+
+visited = [False]*(n+1)
+distance = [0]*(n+1)
+
+def dfs(node, total):
+    for leaf, dist in edges[node]:
+        if not visited[leaf]:
+            visited[leaf] = True
+            distance[leaf] = total + dist
+            dfs(leaf, distance[leaf])
+    
+
+def find_farthest_dist(x):
+    for i in range(1, n+1):
+        visited[i] = False
+        distance[i] = 0
+    
+    visited[x] = True
+    dfs(x, 0)
+
+    return max(distance)
+
+    
+for i in range(1, n+1):
+    farthest_dist[i] = find_farthest_dist(i)
+print(min(farthest_dist[1:]))
