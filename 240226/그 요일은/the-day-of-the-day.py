9abb028737cb5b339b4e62b2c9db0c29f21993b5
Jiyoung Lee <79441145+jiyoungzero@users.noreply.github.com>
2024-02-26 13:11:23 +0900

    Title: 그 요일은, Time: 110ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240226/그 요일은/the-day-of-the-day.py
@@ -0,0 +1,15 @@
+m1, d1, m2, d2 = map(int, input().split())
+
+num_of_day = [0, 31, 29, 31,30,31,30,31,31,30,31,30,31]
+week = ['Mon', 'Tue', 'Wed', 'Thu','Fri', 'Sat', 'Sun']
+
+def to_day(m, d):
+    result = 0
+    for i in range(1, m):
+        result += num_of_day[i]
+    result += d
+    return result
+
+day1 = to_day(m1, d1)
+day2 = to_day(m2, d2)
+print((day2-day1+1)//7+1)
