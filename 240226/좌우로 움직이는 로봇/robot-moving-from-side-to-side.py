3a224e3900b60b3d3d7cbf6d05a57d2c568fd796
Jiyoung Lee <79441145+jiyoungzero@users.noreply.github.com>
2024-02-26 17:08:18 +0900

    Title: 좌우로 움직이는 로봇, Time: 141ms, Memory: 25MB, Status: Wrong Answer - CodeTree

--- 240226/좌우로 움직이는 로봇/robot-moving-from-side-to-side.py
+++ 240226/좌우로 움직이는 로봇/robot-moving-from-side-to-side.py
@@ -23,16 +23,31 @@ ax, bx = 0, 0
 while True:
     if len(a) == 0 and len(b) == 0:
         break
-
+    a_time, b_time = -1, -1
     if a:
-        at, ad = a.popleft()
-        nxt_ax = ax+at*ad
+        a_time, move_a = a.popleft()
     if b:
-        bt, bd = b.popleft()
-        nxt_bx = bx+bt*bd
+        b_time, move_b = b.popleft()
     
-    if ax != bx and ((ax < bx and nxt_ax >= nxt_bx) or ( ax > bx and nxt_ax <= nxt_bx)):
-        answer += 1
-    ax, bx = nxt_ax, nxt_bx
+    if a_time > 0 and b_time > 0:
+        na = ax + a_time*move_a
+        nb = bx + b_time*move_b
+        if (ax != bx) and ((ax < bx and na >= bx) or (ax > bx and na <= bx)):
+            answer += 1
+        ax = na
+        bx = nb
+            
+
+    elif a_time == -1 and b_time > 0:
+        nb = bx + move_b*b_time
+        if (ax != bx) and (ax == nb):
+            answer += 1
+        bx = nb
+    
+    elif a_time > 0 and b_time == -1:
+        na = ax + move_a*a_time
+        if (ax != bx) and (na == bx):
+            answer += 1
+        ax = na
 
 print(answer)
