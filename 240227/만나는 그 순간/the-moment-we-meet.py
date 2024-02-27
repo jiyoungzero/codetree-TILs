b5c3625c9292980c4cf22697f66acd4cb595e82e
Jiyoung Lee <79441145+jiyoungzero@users.noreply.github.com>
2024-02-27 17:26:43 +0900

    Title: 만나는 그 순간, Time: 103ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240227/만나는 그 순간/the-moment-we-meet.py
@@ -0,0 +1,38 @@
+# 시간과 위치가 같아져야 함!
+import sys
+input = sys.stdin.readline
+
+
+N, M = map(int, input().split())
+a, b = [], []
+answer = 0
+
+start = 0
+time = 0
+for _ in range(N):
+    d, t = map(str, input().split())
+    for _ in range(int(t)):
+        if d == 'L':
+            start -= 1
+            a.append(start)
+        else:
+            start += 1
+            a.append(start)
+
+
+start = 0
+time = 0
+for _ in range(M):
+    d, t = map(str, input().split())
+    for _ in range(int(t)):
+        if d == 'L':
+            start -= 1
+            b.append(start)
+        else:
+            start += 1
+            b.append(start)
+
+for i, (a_pos, b_pos) in enumerate(zip(a, b)):
+    if a_pos == b_pos:
+        print(i+1)
+        break
