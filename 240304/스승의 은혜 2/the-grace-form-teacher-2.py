f038d862fd7879915f33687b3778bd2ccaac04da
Jiyoung Lee <79441145+jiyoungzero@users.noreply.github.com>
2024-03-04 01:10:44 +0900

    Title: 스승의 은혜 2, Time: 113ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240304/스승의 은혜 2/the-grace-form-teacher-2.py
+++ 240304/스승의 은혜 2/the-grace-form-teacher-2.py
@@ -4,18 +4,23 @@ input = sys.stdin.readline
 n, b = map(int, input().split())
 needs = [int(input()) for _ in range(n)]
 answer = 0
+
 for i in range(n):
+    copy_b = b
     tmp = 0
     target = needs[i] // 2
+    if target <= copy_b:
+        tmp += 1
+        copy_b -= target
     for j in range(n):
-        if i == j and target <= b:
-            tmp += 1
-            b -= target
+        if i == j:
+            continue
         else:
-            if b >= needs[j]:
+            if copy_b >= needs[j]:
                 tmp += 1
-                b -= needs[j]
+                copy_b -= needs[j]
             else:
                 continue
+    
     answer = max(answer ,tmp)
 print(answer)
