cc33d25205f07aeae1e32998950659b43c27675a
Jiyoung Lee <79441145+jiyoungzero@users.noreply.github.com>
2024-03-15 00:11:13 +0900

    Title: 최소 와이파이 수, Time: 110ms, Memory: 24MB, Status: Passed - CodeTree

--- 240315/최소 와이파이 수/minimum-number-of-wifi.py
+++ 240315/최소 와이파이 수/minimum-number-of-wifi.py
@@ -1,32 +1,11 @@
-import sys
-input =sys.stdin.readline 
+n, m = map(int, input().split())
+arr= list(map(int, input().split()))
 
-n,m = map(int, input().split())
-arr = list(map(int, input().split()))
-answer = 0
-
-idx = 0
-while idx < n:
-    flag = False
-    if arr[idx] == 0:
-        idx += 1
+cnt, i = 0,0
+while i < n :
+    if arr[i] == 0:
+        i += 1
     else:
-        for i in range(idx, min(n, idx + 2*m+ 1)):
-            if arr[i] == 1:
-                flag = True
-        if flag:
-            answer += 1
-            for i in range(idx, min(n, idx + 2*m+1)):
-                arr[i] = 0
-            idx = min(idx + 2*m+1, n-1)
-        elif not flag:
-            idx = min(idx+1, n-1)
-        # print(arr)
-        if idx == n-1:
-            break
-    
-if arr[-1] == 1:
-    answer += 1
-            
-
-print(answer)
+        cnt += 1
+        i += (2*m+1)
+print(cnt)
