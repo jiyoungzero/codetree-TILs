4dc80d0545ed17f4f7c0809fd8c79c612197fbcd
Jiyoung Lee <79441145+jiyoungzero@users.noreply.github.com>
2024-03-16 01:29:08 +0900

    Title: 소괄호 매칭 수 최대화하기, Time: 468ms, Memory: 34MB, Status: Passed - CodeTree

--- 240316/소괄호 매칭 수 최대화하기/maximize-the-number-of-parenthesis-matches.py
+++ 240316/소괄호 매칭 수 최대화하기/maximize-the-number-of-parenthesis-matches.py
@@ -1,40 +1,40 @@
 import sys
 input = sys.stdin.readline 
+from functools import cmp_to_key
 
 n = int(input())
 lsts = [input().rstrip() for _ in range(n)]
 answer = 0
 
-def count_answer(case_):
-    global answer
-    len_ = len(case_)
-    for i in range(len_):
-        if case_[i] == '(':
-            for j in range(i+1, len_):
-                if case_[j] == ")":
-                    answer += 1
-
 open_lst = []
 for lst in lsts:
     open_ = 0
     close_ = 0
-    first_open_idx = 0
-    first_close_idx = 0
     for i in range(len(lst)):
         if lst[i] == '(':
             open_ += 1
-            if open_ == 1:
-                first_open_idx = i
         else:
             close_ += 1
-            if close_ == 1:
-                first_close_idx = i
-    open_lst.append((open_//max(1, close_), close_, first_open_idx, first_close_idx, lst))
-open_lst.sort(key = lambda x: ( -x[0], x[2], x[1], -x[3]))
-case_ = ""
-for ele in open_lst:
-    case_ += ele[4]
+            answer += open_
+    open_lst.append((open_, close_))
 
-# print(case_)
-count_answer(case_)
+def compare(a1, a2):
+    open1, close1 = a1
+    open2, close2 = a2
+    if open1*close2 > open2*close1: # 현재 순서가 맞음
+        return -1
+    if open1*close2 < open2*close1:
+        return 1
+    return 0 # 우선순위가 동일한 경우
+
+
+
+open_lst.sort(key = cmp_to_key(compare))
+open_cnt = 0
+for open_, close_ in open_lst:
+    answer += (open_cnt*close_)
+    open_cnt += open_
+    
+
+
 print(answer)
