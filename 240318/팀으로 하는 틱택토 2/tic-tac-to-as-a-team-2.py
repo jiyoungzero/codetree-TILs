8f3737d52a71eaf64ad54a3842a0d548fb552731
Jiyoung Lee <79441145+jiyoungzero@users.noreply.github.com>
2024-03-18 22:10:05 +0900

    Title: 팀으로 하는 틱택토 2, Time: 101ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240318/팀으로 하는 틱택토 2/tic-tac-to-as-a-team-2.py
@@ -0,0 +1,25 @@
+arr = [list(map(int, input())) for _ in range(3)]
+win_set = []
+
+# # 행
+for i in range(3):
+    tmp_set = set()
+    for j in range(3):
+        tmp_set.add(arr[i][j])
+    if len(tmp_set) == 2 and list(tmp_set) not in win_set:
+        win_set.append(tmp_set)
+# 열 
+for j in range(3):
+    tmp_set = set()
+    for i in range(3):
+        tmp_set.add(arr[i][j])
+    if len(tmp_set) == 2 and list(tmp_set) not in win_set:
+        win_set.append(tmp_set)
+
+# 대각선
+if len(set([arr[0][2], arr[1][1], arr[2][0]])) == 2 and set([arr[0][2], arr[1][1], arr[2][0]]) not in win_set:
+    win_set.append(set([arr[0][2], arr[1][1], arr[2][0]]))
+if len(set([arr[0][0], arr[1][1], arr[2][2]])) == 2 and set([arr[0][0], arr[1][1], arr[2][2]]) not in win_set:
+    win_set.append(set([arr[0][0], arr[1][1], arr[2][2]]))
+
+print(len(win_set))
