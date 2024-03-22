d6bf4ac3a28822c14e36f2f708eb7324e8a0d4d5
Jiyoung Lee <79441145+jiyoungzero@users.noreply.github.com>
2024-03-22 16:10:25 +0900

    Title: 안전 지대, Time: 1282ms, Memory: 148MB, Status: Passed - CodeTree

--- 240322/안전 지대/comfort-zone.py
+++ 240322/안전 지대/comfort-zone.py
@@ -1,42 +1,31 @@
 import sys
 input = sys.stdin.readline 
-from collections import deque
 
 n, m = map(int, input().split())
 arr = [list(map(int, input().split())) for _ in range(n)]
-max_k = 0
+k = 0
 comfort_zone = -1
-dxs, dys = [0, 0, 1, -1], [1, -1, 0, 0]
-def in_range(x, y):
-    return 0 <= x < n and 0 <= y < m
+dxs, dys = [0,0,1,-1], [1,-1,0,0]
 
-def bfs(rain_h, sx, sy):
-    global visited
-    que = deque()
-    que.append((sx, sy))
-
-    visited[sx][sy] = True
-    while que:
-        x, y = que.popleft()
+def dfs(rain_h, x, y):
+    if not (0<=x<n and 0<=y<m):return False
+    if not visited[x][y] and arr[x][y] > rain_h:
+        visited[x][y] = True
         for dir in range(4):
-            nx, ny = x+ dxs[dir], y + dys[dir]
-            if not in_range(nx, ny):continue
-            if not visited[nx][ny] and arr[nx][ny] > rain_h:
-                visited[nx][ny] = True
-                que.append((nx, ny))
-                
+            nx, ny = x + dxs[dir], y + dys[dir]
+            dfs(rain_h, nx, ny)
+        return True
+    return False
 
 for rain_h in range(100, 0, -1):
-    visited = [[False]*m for _ in range(n)]
     tmp_comfort_zone = 0
+    visited = [[False]*m for _ in range(n)]
     for i in range(n):
         for j in range(m):
-            if not visited[i][j] and arr[i][j] > rain_h:
-                bfs(rain_h, i, j)
+            if dfs(rain_h, i, j):
                 tmp_comfort_zone += 1
-    if comfort_zone <= tmp_comfort_zone:
+    if tmp_comfort_zone >= comfort_zone:
         comfort_zone = tmp_comfort_zone
-        max_k = rain_h
-        # print("rain_h", rain_h, tmp_comfort_zone)
+        k = rain_h
 
-print(max_k, comfort_zone)
+print(k, comfort_zone)
