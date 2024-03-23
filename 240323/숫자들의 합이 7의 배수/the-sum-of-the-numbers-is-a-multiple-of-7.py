a66d244298fcbac43a288500264f50d538af3d6c
Jiyoung Lee <79441145+jiyoungzero@users.noreply.github.com>
2024-03-23 22:31:44 +0900

    Title: 숫자들의 합이 7의 배수, Time: 5084ms, Memory: 26MB, Status: Time Limit Exceed - CodeTree

--- 240323/숫자들의 합이 7의 배수/the-sum-of-the-numbers-is-a-multiple-of-7.py
+++ 240323/숫자들의 합이 7의 배수/the-sum-of-the-numbers-is-a-multiple-of-7.py
@@ -1,12 +1,14 @@
 n = int(input())
 arr = [int(input()) for _ in range(n)]
 answer = 0
+dp = [0]*(n+1)
+dp[1] = arr[0]
 
-for i in range(n):
-    tmp_cnt = 0
-    tmp_sum = 0
-    for j in range(i+1, n):
-        if sum(arr[i:j])%7 == 0:
-            tmp_cnt = (j-i)
-            answer = max(answer, tmp_cnt)
+for i in range(2, n+1):
+    dp[i] = arr[i-1]+dp[i-1]
+
+for a in range(1, n+1):
+    for b in range(a+1, n+1):
+        if (dp[b]-dp[a-1])%7 == 0:
+            answer = max(answer, b-a+1)
 print(answer)
