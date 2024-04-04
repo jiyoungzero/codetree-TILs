22c0b72bb22d251726c67113bb67c23420186f7e
Jiyoung Lee <79441145+jiyoungzero@users.noreply.github.com>
2024-04-04 23:56:03 +0900

    Title: 토끼와 경주, Time: 1387ms, Memory: 29MB, Status: Passed - CodeTree

--- 240404/토끼와 경주/rabit-and-race.py
+++ 240404/토끼와 경주/rabit-and-race.py
@@ -1,119 +1,119 @@
+from collections import defaultdict
+import heapq
 import sys
 input = sys.stdin.readline 
-from collections import defaultdict
 
 Q = int(input())
-cmds = []
-for _ in range(Q):
-    cmds.append(list(map(int, input().split())))
-N, M, P, K, S, L = 0,0,0,0,0,0
-rabbits = [] # 고유번호, 이동거리
-counts = {}
-scores = {}
-pos = {}
+N, M, P = 0, 0, 0
+heap = []
+dist = defaultdict()
+score = defaultdict()
+minus_score = 0
+dxs, dys = [-1, 1, 0, 0],[0, 0, -1, 1]
 
-N, M, P = cmds[0][1], cmds[0][2], cmds[0][3]
-lst = cmds[0][4:]
-for i in range(len(lst)//2):
-    a, b = lst.pop(), lst.pop()
-    rabbits.append((b, a))
-for rabbit in rabbits:
-    pos[rabbit[0]] = (0,0)
-    counts[rabbit[0]] = 0
-    scores[rabbit[0]] = 0
-
+def init(cmd):
+    global N, M, P
+    N = cmd[0]
+    M = cmd[1]
+    P = cmd[2]
+    for i in range(3, len(cmd)):
+        if i%2 == 1:
+            heapq.heappush(heap, [0,0,0,0,cmd[i]])
+            dist[cmd[i]] = cmd[i+1]
+            score[cmd[i]] = 0
+    return 
 
 def in_range(x, y):
     return 0 <= x < N and 0 <= y < M 
 
-def match_pid_dist(pid):
-    for rabbit in rabbits:
-        if rabbit[0] == pid:
-            return rabbit[1]
-    return False
+def out_of_range(nx, ny):
+    nx %= 2*(N-1)
+    ny %= 2*(M-1)
+    return min(nx, 2*(N-1)-nx), min(ny, 2*(M-1)-ny)
 
-def pick_move_rabbit():
-    global counts
-    lst = []
-    for k, v in counts.items():
-        lst.append((v, pos[k][0]+pos[k][1], pos[k][0], pos[k][1], k))
-    lst.sort(key = lambda x:(x[0], x[1], x[2], x[3], x[4]))
-    target_pid = lst[0][-1]
-    counts[target_pid] += 1
+def most_row_col_pos(positions):
+    result = []
+    positions.sort(key = lambda x : -(x[0]+x[1]))
+    max_value = positions[0][0] + positions[0][1]
+    for position in positions:
+        if position[0]+position[1] == max_value:
+            result.append(position)
+        else:break
+    return result
 
-    return target_pid
+def most_row(positions):
+    result = []
+    positions.sort(key = lambda x : -x[0])
+    max_value = positions[0][0]
+    for position in positions:
+        if position[0] == max_value:
+            result.append(position)
+        else:break
+    return result 
 
-def move(rabbit_pid):
-    global pos, scores, rabbits, counts
+def most_col(positions):
+    positions.sort(key = lambda x : -x[1])
+    return positions[0]
+
+def select_position(positions):
+    positions = most_row_col_pos(positions)
+    if len(positions) == 1:
+        return positions[0]
     
-    dxs, dys = [-1, 0, 1, 0],[0, 1, 0, -1] # 상 우 하 좌
-    d_lst = []
+    positions = most_row(positions)
+    if len(positions) == 1:
+        return positions[0]
     
-    for dir in range(4):
-        x, y = pos[rabbit_pid]
-        dx, dy = dxs[dir], dys[dir]
-        for dist in range(match_pid_dist(rabbit_pid)):
-            if not in_range(x + dx, y + dy):
-                dx = -dx
-                dy = -dy
-            nx, ny = x + dx, y + dy
-            x, y = nx, ny
-        d_lst.append((x+y, x, y))
-    d_lst.sort(key = lambda x :(-x[0], -x[1], -x[2]))
-    final_x, final_y = d_lst[0][1], d_lst[0][2]
-    pos[rabbit_pid] = (final_x, final_y)
+    positions = most_col(positions)
+    return positions
     
-    # pid를 제외한 토끼의 점수 얻기
-    for k in scores.keys():
-        if k == rabbit_pid:continue
-        else:
-            scores[k] += (final_x+final_y+2)
+def addScores(nx, ny, id):
+    global minus_score
+    minus_score += (-nx-ny-2)
+    score[id] += (-nx-ny-2)
+
+def simulate(cmd):
+    K, S = cmd[0], cmd[1]
+
+    select_rabbit = []
+    for _ in range(K):
+        jump, s, x, y, pid = heapq.heappop(heap)
+        jump += 1
+        
+        positions = []
+        for i in range(4):
+            nx, ny = x + dxs[i]*dist[pid], y+dys[i]*dist[pid]
+            if not in_range(nx, ny):
+                nx, ny = out_of_range(nx, ny)
+            positions.append((nx, ny))
+        nx, ny = select_position(positions)
+        addScores(nx, ny, pid)
+        heapq.heappush(select_rabbit, [-(nx+ny), -nx, -ny, -pid])
+        heapq.heappush(heap, [jump, nx+ny, nx, ny, pid])
+    _,_,_, pid = heapq.heappop(select_rabbit)
+
+    score[-pid] += S 
     return 
-            
-def get_S_score():
-    lst = []
-    for k, v in pos.items():
-        lst.append((v[0]+v[1], v[0], v[1], k, counts[k]))
-    lst.sort(key = lambda x : (-x[0], -x[1], -x[2], -x[3]))
-    cur = 0
-    flag = False
-    while lst[cur][-1] == 0:
-        cur += 1
-        flag = True
-    if flag: cur -= 1 
 
-    target_pid = lst[cur][3]
-    scores[target_pid] += S 
+def changeDist(cmd):
+    pid, L = cmd[0], cmd[1]
+    dist[pid] *= L 
     return 
-       
 
-def simulate():
-    global K
-    for time in range(K):
-        rabbit_pid = pick_move_rabbit()
-        # print(time, "번째 선택 =", rabbit_pid)
-        move(rabbit_pid)
-        # print("이동 후 = ", pos, " 점수 =", scores)
-    get_S_score()
-    # print("scores : ", scores)
-    # print("counts :", counts)
-    # print("pos :", pos)
+def get_max_score():
+    result = -1
+    for s in score.values():
+        if s > result:
+            result = s 
+    return result - minus_score
 
-
-for c in range(Q):
-    if cmds[c][0] == 100:
-        continue
-    elif cmds[c][0] == 200:
-        K, S = cmds[c][1], cmds[c][2]
-        simulate()
-    elif cmds[c][0] == 300:
-        pid_target, L = cmds[2][1], cmds[2][2]
-        for i, rabbit in enumerate(rabbits):
-            if rabbit[0] == pid_target:
-                rabbits[i] = (pid_target, L*rabbit[1])
+for _ in range(Q):
+    cmd = list(map(int, input().split()))
+    if cmd[0] == 100:
+        init(cmd[1:])
+    elif cmd[0] == 200:
+        simulate(cmd[1:])
+    elif cmd[0] == 300:
+        changeDist(cmd[1:])
     else:
-        max_score = 0
-        for tmp in scores.values():
-            if tmp > max_score:
-                max_score = tmp
-        print(max_score)
+        print(get_max_score())
