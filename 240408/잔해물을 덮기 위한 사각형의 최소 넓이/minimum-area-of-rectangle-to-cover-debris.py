0e7a1adad4100ee97c22ba98b0075a4ac075c5de
Jiyoung Lee <79441145+jiyoungzero@users.noreply.github.com>
2024-04-08 16:30:15 +0900

    Title: 잔해물을 덮기 위한 사각형의 최소 넓이, Time: 159ms, Memory: 55MB, Status: Wrong Answer - CodeTree

---
+++ 240408/잔해물을 덮기 위한 사각형의 최소 넓이/minimum-area-of-rectangle-to-cover-debris.py
@@ -0,0 +1,25 @@
+ax1, ay1, ax2, ay2 = map(int ,input().split())
+bx1, by1, bx2, by2 = map(int, input().split())
+
+OFFSET = 1000
+arr = [[0]*2001 for _ in range(2001)]
+
+for i in range(ax1+OFFSET, ax2+OFFSET):
+    for j in range(ay1+OFFSET, ay2+OFFSET):
+        arr[i][j] = 1
+
+for i in range(bx1+OFFSET, bx2+OFFSET):
+    for j in range(by1+OFFSET, by2+OFFSET):
+        if arr[i][j] == 1:
+            arr[i][j] = 0
+
+min_x, min_y, max_x, max_y = 2001, 2001, -2001, -2001
+for i in range(2001):
+    for j in range(2001):
+        if arr[i][j]:
+            min_x = min(i, min_x)
+            min_y = min(j, min_y)
+            max_x = max(i, max_x)
+            max_y = max(j, max_y)
+# print(min_x, min_y, max_x, max_y)
+print((max_x-min_x+1)*(max_y-min_y+1))
