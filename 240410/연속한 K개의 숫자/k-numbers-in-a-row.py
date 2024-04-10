4dfa64e4f9e6d2f5a379d9bbdd6c33082b1713f1
Jiyoung Lee <79441145+jiyoungzero@users.noreply.github.com>
2024-04-10 16:53:07 +0900

    Title: 연속한 K개의 숫자, Time: 124ms, Memory: 27MB, Status: Passed - CodeTree

--- 240410/연속한 K개의 숫자/k-numbers-in-a-row.py
+++ 240410/연속한 K개의 숫자/k-numbers-in-a-row.py
@@ -3,31 +3,16 @@ input = sys.stdin.readline
 
 n, k, b = map(int, input().split())
 blank = [int(input()) for _ in range(b)]
-
-arr = [1 for i in range(1, n+1)]
+answer = int(1e9)
+arr = [0 for i in range(1, n+1)]
 arr = [0]+arr
 for ele in blank:
-    arr[ele] = 0
-# print(arr)
+    arr[ele] = 1
 
-
-# 가능한 최대의 연속 수 
-# max_value = 0;
-# for i in range(1, n+1):
-#     max_value = max(max_value, )
-p_prefix = [0]*(n+1)
-nums = [i for i in range(n+1)]
-
+prefix = [0]*(n+1)
 for i in range(1, n+1):
-    p_prefix[i] = p_prefix[i-1] + arr[i]
+    prefix[i] = prefix[i-1]+arr[i]
 
-
-prefix = [0]*(n-k+1)
-for i in range(n-k+1):
-    cnt = 0
-    for j in range(i, i+k):
-        if arr[j]:
-            cnt += 1
-    prefix[i] = cnt
-
-print(k - max(prefix))
+for i in range(1, n-k+2):
+    answer = min(answer, prefix[i+k-1]-prefix[i-1])
+print(answer)
