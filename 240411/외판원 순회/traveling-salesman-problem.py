bf7d769f7a26ec7de587e5bf2db0488f99361307
Jiyoung Lee <79441145+jiyoungzero@users.noreply.github.com>
2024-04-11 16:20:18 +0900

    Title: 외판원 순회, Time: 508ms, Memory: 28MB, Status: Passed - CodeTree

--- 240411/외판원 순회/traveling-salesman-problem.py
+++ 240411/외판원 순회/traveling-salesman-problem.py
@@ -8,11 +8,13 @@ answer = int(1e9)
 
 def get_cost(lst):
     result = 0
+    if graph[0][lst[0]] == 0 or graph[lst[-1]][0] == 0:
+        return False
     result += graph[0][lst[0]] 
     result += graph[lst[-1]][0]
 
     for i in range(len(lst)-1):
-        if graph[lst[i]][lst[i+1]]==0:
+        if graph[lst[i]][lst[i+1]] == 0:
             return False
         else:
             result += graph[lst[i]][lst[i+1]]
@@ -24,7 +26,7 @@ def backtracking(depth, cur):
     global answer
     if len(cur) == n-1:
         # 비용 구하기
-        if get_cost(cur):
+        if get_cost(cur) != False:
             answer = min(answer, get_cost(cur))
         return
 
