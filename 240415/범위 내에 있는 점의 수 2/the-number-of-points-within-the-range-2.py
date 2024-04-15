a22c442efbf923b50f63fd1099af5f2298b962bb
Jiyoung Lee <79441145+jiyoungzero@users.noreply.github.com>
2024-04-15 13:15:35 +0900

    Title: 범위 내에 있는 점의 수 2, Time: 5049ms, Memory: 34MB, Status: Time Limit Exceed - CodeTree

---
+++ 240415/범위 내에 있는 점의 수 2/the-number-of-points-within-the-range-2.py
@@ -0,0 +1,16 @@
+import sys
+input = sys.stdin.readline 
+
+n, q = map(int ,input().split())
+dot_pos = list(map(int, input().split()))
+MAX_N = 1000001
+prefix = [0]*MAX_N
+for i in range(1, MAX_N):
+    if i in dot_pos:
+        prefix[i] = prefix[i-1]+1
+    else:
+        prefix[i] = prefix[i-1]
+
+for _ in range(q):
+    a, b = map(int, input().split())
+    print(prefix[b]-prefix[a-1])
