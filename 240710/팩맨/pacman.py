f5451ba38d9d951682ceb942cf9b29a266878974
Jiyoung Lee <79441145+jiyoungzero@users.noreply.github.com>
2024-07-10 23:59:18 +0900

    Title: 팩맨, Time: 300ms, Memory: 28MB, Status: Passed - CodeTree

--- 240710/팩맨/pacman.py
+++ 240710/팩맨/pacman.py
@@ -1,157 +1,137 @@
 import sys
 input = sys.stdin.readline 
-import copy
 
-m, t = map(int, input().split())
-r, c = map(int, input().split())
-pecman = [r-1, c-1]
-monsters = []
-dead = [] # 몬스터 시체
+n = 4
+m, t = map(int ,input().split())
+t_num = 1 # 현재 턴
+
+px, py = map(int ,input().split())
+px -= 1
+py -= 1
+
+# 몬스터를 위한 방향
+dxs = [-1, -1,  0,  1, 1, 1, 0, -1]
+dys = [ 0, -1, -1, -1, 0, 1, 1,  1]
+
+# 팩맨을 위한 방향 : 상좌하우 순
+p_dxs = [-1,  0, 1, 0]
+p_dys = [ 0, -1, 0, 1]
+
+
+# t턴에 (x, y) 위치의 move_dir를 바라보는 몬스터의 수
+monster = [
+    [
+        [
+            [0]*8
+            for _ in range(n)
+        ]
+        for _ in range(n)
+    ]
+
+    for _ in range(26)
+]
+
+# (x, y)위치에서 썩기 몇 초 전인 시체의 수
+dead = [
+    [
+        [0]*3
+        for _ in range(n)
+    ]
+
+    for _ in range(n)
+]
+
 for _ in range(m):
-    r, c, d = map(int, input().split())
-    monsters.append((r-1, c-1, d-1))
-arr = [[0]*4 for _ in range(4)] 
-
-
-dxs, dys = [-1, -1, 0, 1, 1, 1, 0, -1], [0, -1, -1, -1, 0 ,1, 1, 1] # 위, 위-왼쪽, 왼쪽, 아래-왼쪽, 아래...
-p_dxs, p_dys = [-1, 0, 1, 0], [0, -1, 0, 1] # 상, 좌, 하, 우
-routes = []
-
-def dfs(tmp, routes):
-    if len(tmp) == 3:
-        routes.append(tmp[:])
-        return 
-
-    for dir in range(4):
-        tmp.append((p_dxs[dir], p_dys[dir]))
-        dfs(tmp, routes)
-        tmp.pop()   
-
-dfs([], routes) # routes = [[(-1, 0), (-1, 0), (-1, 0)], [(-1, 0), (-1, 0), (0, -1)],...     
-
-def can_go(nx, ny):
-    if not in_range(nx, ny):return False
-    if [nx, ny] == pecman: return False
-    for d in dead:
-        deadx, deady, _ = d
-        if nx == deadx and ny == deady:
-            return False
-    return True
+    mx, my, mdir = map(int, input().split())
+    monster[0][mx-1][my-1][mdir-1] += 1
 
 def in_range(x, y):
-    return 0 <= x < 4 and 0 <= y < 4
+    return 0 <= x and x < n and 0 <= y and y < n
 
+def can_go(x,y):
+    return in_range(x, y) and dead[x][y][0] == 0 and dead[x][y][1] == 0 and (x, y) != (px, py)
 
-# 몬스터 시체가 있거나, 팩맨이 있는 경우거나 격자를 벗어나는 방향일 경우에는 반시계 방향으로 45도
-# 만약 8 방향을 다 돌았는데도 불구하고, 모두 움직일 수 없었다면 해당 몬스터는 움직이지 않습니다.
-def move_monsters():
-    global monsters
-    for idx, monster in enumerate(monsters):
-        x, y, d = monster
-        go = False
-        original_d = d
-        nx, ny = x + dxs[d], y + dys[d]
-        for _ in range(8):
-            if can_go(nx, ny):
-                go = True
-                monsters[idx] = [nx, ny, d]
-                break            
-            d = (d + 1)%8
-            nx, ny = x + dxs[d], y + dys[d]
+def get_nxt_pos(x, y, move_dir):
+    for _ in range(8):
+        nx, ny = x+dxs[move_dir], y+ dys[move_dir]
+        if can_go(nx, ny):
+            return (nx, ny, move_dir)
+        move_dir = (move_dir+1)%8
+    return (x, y, move_dir)
 
-        if not go and d == original_d:
-            continue
-                
+def move_monster():
+    for i in range(n):
+        for j in range(n):
+            for k in range(8):
+                x, y, n_dir = get_nxt_pos(i, j, k)
+                monster[t_num][x][y][n_dir] += monster[t_num-1][i][j][k] 
+                    
+def get_killed_num(d1, d2, d3):
+    x, y = px, py
+    k_num = 0
 
-def count_monster(paths):
-    result = 0
-    for path in paths:
-        x, y = path
-        for monster in monsters:
-            mx, my, _ = monster
-            if [x, y] == [mx, my]:
-                result += 1
-    return result
+    visited = []
+    for move_dir in [d1, d2, d3]:
+        nx, ny = x + p_dxs[move_dir], y + p_dys[move_dir]
+        if not in_range(nx, ny): return -1
+        if (nx, ny) not in visited:
+            visited.append((nx, ny))
+            k_num += sum(monster[t_num][nx][ny])
+        x, y = nx, ny
+    return k_num
 
-def move_pecman(time):
-    global monsters, dead, pecman
-    eat = [-1]*64
+def kill(best_route):
+    global px, py
     
-    for i, route in enumerate(routes):
-        x, y = pecman
-        paths = set()
-        flag = True
-        for dx, dy in route:
-            nx, ny = x + dx, y + dy
-            if in_range(nx, ny):
-                paths.add((nx, ny))
-                x, y = nx, ny
-            else: 
-                flag = False
-                break
-        if flag:
-            eat[i] = count_monster(paths)
-    
-    max_cnt, max_idx = -1, -1
-    for i in range(64):
-        if eat[i] > max_cnt:
-            max_cnt = eat[i]
-            max_idx = i
-    
-    final_route = routes[max_idx]
-    # print("현재 팩맨", pecman, " 최종 경로 ->", final_route, max_cnt, max_idx)
-    x, y = pecman
-    eat_idx = set()
-    for dx, dy in final_route:
-        x += dx
-        y += dy
-        for i, monster in enumerate(monsters):
-            if monster[0] == x and monster[1] == y:
-                dead.append([monsters[i][0], monsters[i][1], time+2])
-                eat_idx.add(i)
-    n_monsters = []
-    for idx in range(len(monsters)):
-        if idx not in eat_idx:
-            n_monsters.append(monsters[idx])
-    
-    # 몬스터 정보 업데이트
-    monsters = copy.deepcopy(n_monsters)
+    d1, d2, d3 = best_route
+    for move_dir in [d1, d2, d3]:
+        nx, ny = px + p_dxs[move_dir], py + p_dys[move_dir]
+        dead[nx][ny][2] += sum(monster[t_num][nx][ny])
 
-    # 팩맨 위치 업데이트
-    pecman = [x, y]
-    return 
+        for i in range(8):
+            monster[t_num][nx][ny][i] = 0
+        px, py = nx, ny
 
+def move_pecman():
+    max_cnt = -1
+    best_route = (-1, -1, -1)
 
-def born_dup(lst):
-    global monsters
-    for x, y, d in lst:
-        monsters.append([x, y, d])
+    for i in range(4):
+        for j in range(4):
+            for k in range(4):
+                m_cnt = get_killed_num(i, j, k)
+                if m_cnt > max_cnt:
+                    max_cnt = m_cnt
+                    best_route = (i, j, k)
+    kill(best_route)
 
+def dead_monster():
+    for i in range(n):
+        for j in range(n):
+            for k in range(2):
+                dead[i][j][k] = dead[i][j][k+1]
+            dead[i][j][2] = 0
 
-for time in range(t):
-    duplicate = copy.deepcopy(monsters)
-    # print(time, "번 째 : 복제 ", duplicate)
-    move_monsters() 
-    # print("몬스터 이동 후 : ", monsters)
-    move_pecman(time)
-    # print("팩맨 이동 후 :", pecman)
-    # print("팩맨 이동 후 몬스터 상태 :", monsters)
-    born_dup(duplicate)
-    # print("알 부화 :", duplicate, ", 최종 몬스터 : ", monsters)
+def duplicate_monster():
+    for i in range(n):
+        for j in range(n):
+            for k in range(8):
+                monster[t_num][i][j][k] += monster[t_num-1][i][j][k]
 
-    # 시체 처리
-    n_dead = []
-    erase = []
-    for i, d in enumerate(dead):
-        x, y, tt = d
-        if tt == time:
-            erase.append(i)
-    for i in range(len(dead)):
-        if i not in erase:
-            n_dead.append(dead[i])
-    dead = copy.deepcopy(n_dead)
+def simulate():
+    move_monster()
+    move_pecman()
 
-    # print("dead :", dead)
-    # print() 
+    dead_monster()
+    duplicate_monster()
 
-print(len(monsters))
+while t_num <= t:
+    simulate()
+    t_num += 1
+
+answer = 0
+for i in range(n):
+    for j in range(n):
+        for k in range(8):
+            answer += monster[t][i][j][k]
+print(answer)
