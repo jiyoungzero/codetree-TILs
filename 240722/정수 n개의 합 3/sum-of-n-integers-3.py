6856dc4bce50ce2a9f5f517cecab38f666bd538d
Jiyoung Lee <79441145+jiyoungzero@users.noreply.github.com>
2024-07-22 15:52:11 +0900

    Title: 정수 n개의 합 3, Time: 151ms, Memory: 27MB, Status: Passed - CodeTree

--- 240722/정수 n개의 합 3/sum-of-n-integers-3.py
+++ 240722/정수 n개의 합 3/sum-of-n-integers-3.py
@@ -14,11 +14,10 @@ for i in range(1, n+1):
     for j in range(1, n+1):
         prefix[i][j] = prefix[i-1][j] + prefix[i][j-1] - prefix[i-1][j-1] + arr[i-1][j-1]
 
+def get_sum(x1, y1, x2, y2):
+    return prefix[x2][y2] - prefix[x1-1][y2] - prefix[x2][y1-1] + prefix[x1-1][y1-1]
 
-
-for i in range(n-k+1):
-    for j in range(n-k+1):
-        sx, sy = i, j
-        ex, ey = i+k, j+k
-        answer = max(answer, prefix[ex][ey] - prefix[sx][ey] - prefix[ex][sy] + prefix[sx][sy])
+for i in range(1, n-k+2):
+    for j in range(1, n-k+2):
+        answer = max(answer, get_sum(i, j, i+k-1, j+k-1))
 print(answer)
