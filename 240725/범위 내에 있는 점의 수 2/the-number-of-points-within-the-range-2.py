ed985f8a91d0e442b094c63b5267014fe21a633f
Jiyoung Lee <79441145+jiyoungzero@users.noreply.github.com>
2024-07-25 00:35:52 +0900

    Title: 범위 내에 있는 점의 수 2, Time: 242ms, Memory: 37MB, Status: Passed - CodeTree

--- 240725/범위 내에 있는 점의 수 2/the-number-of-points-within-the-range-2.py
+++ 240725/범위 내에 있는 점의 수 2/the-number-of-points-within-the-range-2.py
@@ -3,18 +3,18 @@ input = sys.stdin.readline
 from collections import defaultdict
 
 n, q = map(int, input().split())
-nums = defaultdict(bool)
-tmp = list(map(int, input().split()))
-tmp.sort()
-for ele in tmp:
-    nums[ele] = True
+nums =list(map(int, input().split()))
+
 prefix = [0]*(1000001)
+for num in nums:
+    prefix[num] += 1
+
 for i in range(1, 1000001):
-    if nums[i]:
-        prefix[i] = prefix[i-1] + 1
-    else:
-        prefix[i] = prefix[i-1]
+    prefix[i] += prefix[i-1] 
         
 for _ in range(q):
     s, e = map(int, input().split())
-    print(prefix[e]-prefix[s-1])
+    if s == 0:
+        print(prefix[e])
+    else:
+        print(prefix[e]-prefix[s-1])
