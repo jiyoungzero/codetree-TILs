ef9bd913f740d3a126566a655ea49f8cf9becdcb
Jiyoung Lee <79441145+jiyoungzero@users.noreply.github.com>
2024-07-25 16:44:37 +0900

    Title: 최대 직사각형 합, Time: 104ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240725/최대 직사각형 합/max-rect-sum-in-grid.py
+++ 240725/최대 직사각형 합/max-rect-sum-in-grid.py
@@ -4,26 +4,22 @@ input = sys.stdin.readline
 n = int(input())
 arr = [list(map(int, input().split())) for _ in range(n)]
 prefix = [[0]*(1+n) for _ in range(n+1)]
-prefix[1][1] = arr[0][0]
 
-for i in range(n):
-    prefix[1][i+1] = prefix[1][i] + arr[0][i]
-for i in range(1, n):
-    prefix[i+1][1] = prefix[i][1] + arr[i][0]
-
-
-for i in range(2, n+1):
-    for j in range(2, n+1):
+prefix[0][0] = 0
+for i in range(1, n+1):
+    for j in range(1, n+1):
         prefix[i][j] = prefix[i-1][j] + prefix[i][j-1] - prefix[i-1][j-1] + arr[i-1][j-1]
 
 
+def get_sum(x1, y1, x2, y2):
+    return prefix[x2][y2] - prefix[x1-1][y2] - prefix[x2][y1-1] + prefix[x1-1][y1-1] 
+
 answer = -int(1e9)
-for i in range(1, n+1):
-    for j in range(1, n+1):
-        tmp = 0
-        for s in range(1, n+2-i):
-            for e in range(1, n+2-j):
-                tmp = prefix[s+i-1][e+j-1] - prefix[s+i-1][e-1] - prefix[s-1][e+j-1] + prefix[s-1][e-1]
-                if tmp > answer: 
-                    answer = tmp
+for sr in range(1, n+1):
+    for er in range(sr, n+1):
+        dp = [0]*(n+1) 
+        for y in range(1, n+1):
+            v = get_sum(sr, y, er, y)
+            dp[y] = max(dp[y-1]+v, v)
+        answer = max(answer, max(dp))
 print(answer)
