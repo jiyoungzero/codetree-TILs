ce0b5401e9262a084f2a4c3dab5799bdff9c72d3
Jiyoung Lee <79441145+jiyoungzero@users.noreply.github.com>
2024-07-25 16:48:31 +0900

    Title: 최대 직사각형 합, Time: 489ms, Memory: 27MB, Status: Passed - CodeTree

--- 240725/최대 직사각형 합/max-rect-sum-in-grid.py
+++ 240725/최대 직사각형 합/max-rect-sum-in-grid.py
@@ -11,6 +11,7 @@ for i in range(1, n+1):
         prefix[i][j] = prefix[i-1][j] + prefix[i][j-1] - prefix[i-1][j-1] + arr[i-1][j-1]
 
 
+
 def get_sum(x1, y1, x2, y2):
     return prefix[x2][y2] - prefix[x1-1][y2] - prefix[x2][y1-1] + prefix[x1-1][y1-1] 
 
@@ -21,5 +22,6 @@ for sr in range(1, n+1):
         for y in range(1, n+1):
             v = get_sum(sr, y, er, y)
             dp[y] = max(dp[y-1]+v, v)
-        answer = max(answer, max(dp))
+
+        answer = max(answer, max(dp[1:]))
 print(answer)
