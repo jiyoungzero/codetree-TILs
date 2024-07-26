bab5852ad8a97a6ff61a88f7457f58458e9dfcc9
Jiyoung Lee <79441145+jiyoungzero@users.noreply.github.com>
2024-07-26 15:00:24 +0900

    Title: 삼 오 무, Time: 117ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240726/삼 오 무/three-five-moo.py
@@ -0,0 +1,22 @@
+import sys
+input = sys.stdin.readline 
+
+n = int(input())
+answer= 0
+
+def get_under_nums(mid):
+    cnt = mid//3 + mid //5 - mid//15
+    return mid - cnt
+
+l, r = 1, 10**9
+while l <= r:
+    mid = (l+r)//2
+    under = get_under_nums(mid)
+    if under == n: # 숫자 mid 이하에 있는 숫자의 개수가 n개 인지
+        answer = mid
+        break
+    elif under > n:
+        r = mid - 1
+    else:
+        l = mid + 1
+print(answer)
