c58e5a9cdf0cf535501e153f6df24ba505bce345
Jiyoung Lee <79441145+jiyoungzero@users.noreply.github.com>
2024-07-26 14:07:05 +0900

    Title: 컴퓨터와 함께하는 숫자 게임 2, Time: 189ms, Memory: 28MB, Status: Passed - CodeTree

--- 240726/컴퓨터와 함께하는 숫자 게임 2/play-number-game-with-computer-2.py
+++ 240726/컴퓨터와 함께하는 숫자 게임 2/play-number-game-with-computer-2.py
@@ -2,18 +2,17 @@ import sys
 input = sys.stdin.readline 
 
 m = int(input())
-arr = list(range(1, m+1))
 a, b = map(int, input().split())
 
 
 def binary_search(target):
-    l, r = 0, m-1
+    l, r = 1, m
     result = 0
     while l <= r:
         mid = (l+r)//2
-        if arr[mid] == target:
+        if mid == target:
             return result+1
-        elif arr[mid] < target:
+        elif mid < target:
             l = mid + 1
         else:
             r = mid - 1
