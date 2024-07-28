8fbba963dc9e6506984027897419c3e6f14011c1
Jiyoung Lee <79441145+jiyoungzero@users.noreply.github.com>
2024-07-29 01:19:03 +0900

    Title: 가장 짧은 부분합, Time: 134ms, Memory: 29MB, Status: Wrong Answer - CodeTree

---
+++ 240729/가장 짧은 부분합/shortest-subtotal.py
@@ -0,0 +1,20 @@
+import sys
+input = sys.stdin.readline 
+
+n, s = map(int, input().split())
+arr = list(map(int, input().split()))
+arr = [0] + arr
+answer = 100001
+
+sum_v = 0
+j = 0
+for i in range(1, n+1):
+    while j + 1 < n+1 and sum_v + arr[j+1] < s:
+        sum_v += arr[j+1]
+        j += 1
+    
+    answer = min(answer, j-i+2)
+    # print(answer, "->", j-i+1, "j =", j, "i = ", i, sum_v)
+    sum_v -= arr[i]
+
+print(answer)
