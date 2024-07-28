2fe65d57780cded1070e00d5cf8de956eaebb786
Jiyoung Lee <79441145+jiyoungzero@users.noreply.github.com>
2024-07-29 01:27:12 +0900

    Title: 가장 짧은 부분합, Time: 144ms, Memory: 32MB, Status: Passed - CodeTree

--- 240729/가장 짧은 부분합/shortest-subtotal.py
+++ 240729/가장 짧은 부분합/shortest-subtotal.py
@@ -12,9 +12,10 @@ for i in range(1, n+1):
     while j + 1 < n+1 and sum_v + arr[j+1] < s:
         sum_v += arr[j+1]
         j += 1
-    
-    answer = min(answer, j-i+2)
+
+    if j+1 < n+1 and sum_v + arr[j+1] >= s:
+        answer = min(answer ,j-i+2)
     # print(answer, "->", j-i+1, "j =", j, "i = ", i, sum_v)
     sum_v -= arr[i]
 
-print(answer)
+print(answer if answer < 100001 else -1)
