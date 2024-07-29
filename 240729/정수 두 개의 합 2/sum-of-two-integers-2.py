52d2aac127f9d98ec4796a9a0c64d60e469cc8d5
Jiyoung Lee <79441145+jiyoungzero@users.noreply.github.com>
2024-07-29 15:41:09 +0900

    Title: 정수 두 개의 합 2, Time: 3716ms, Memory: 26MB, Status: Passed - CodeTree

--- 240729/정수 두 개의 합 2/sum-of-two-integers-2.py
+++ 240729/정수 두 개의 합 2/sum-of-two-integers-2.py
@@ -3,14 +3,23 @@ input = sys.stdin.readline
 
 n, k = map(int, input().split())
 arr = [int(input()) for _ in range(n)]
+arr.sort()
 
-j = 0
+
 answer = 0
 for i in range(n):
-    while j+1 < n and arr[i] + arr[j+1] > k:
-        j += 1
-    
-    if j+1 < n and arr[i] + arr[j+1] <= k:
+    j = i
+    while j+1 < n and arr[i] + arr[j+1] <= k:
         answer += 1
+        j += 1
+        # print(i, j)
+        
+        
+    
+    # if j+1 < n and arr[i] + arr[j+1] <= k:
+    #     answer += 1
+    #     print(i, j+1)
+
+
 
 print(answer)
