1b588708d083a89d0d8f9020355e1f7d2ae3cf97
Jiyoung Lee <79441145+jiyoungzero@users.noreply.github.com>
2024-08-05 00:54:44 +0900

    Title: 이상한 폭탄, Time: 230ms, Memory: 32MB, Status: Passed - CodeTree

--- 240805/이상한 폭탄/strange-bomb.py
+++ 240805/이상한 폭탄/strange-bomb.py
@@ -8,11 +8,13 @@ arr = [int(input()) for _ in range(n)]
 nums = set(arr)
 R = [0]*n # 오른쪽의 원소 중 가장 가까이에 잇으면서 동일한 수의 위치
 
-num_dict = defaultdict(int)
+num_dict = dict()
 for i in range(n):
     if arr[i] not in num_dict:
         num_dict[arr[i]] = i 
     else:
         if abs(num_dict[arr[i]] - i) <= k:
             answer = max(answer, arr[i])
+        num_dict[arr[i]] = i
+
 print(answer if answer > 0 else '-1')
