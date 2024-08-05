5e6d38df4bd495af9778971bccb957f676fae128
Jiyoung Lee <79441145+jiyoungzero@users.noreply.github.com>
2024-08-06 00:24:20 +0900

    Title: 0에 가장 가까운 합, Time: 173ms, Memory: 32MB, Status: Passed - CodeTree

--- 240806/0에 가장 가까운 합/sum-closest-to-zero.py
+++ 240806/0에 가장 가까운 합/sum-closest-to-zero.py
@@ -4,18 +4,16 @@ input = sys.stdin.readline
 n = int(input())
 arr = list(map(int, input().split()))
 answer = sys.maxsize
-arr.sort()
-
+arr = [0] + sorted(arr)
 
 # -123 1 1 2 2  124
-e = n-1
-for s in range(n-1):
-    tmp = arr[s] + arr[e]
+e = n
+for s in range(1, n+1):
     if s < e:
-        answer = min(answer, abs(tmp))
+        answer = min(answer, abs(arr[s] + arr[e]))
 
-    while True:
-        if s >= e-1 or (arr[s] + arr[e-1] < 0):break
+    while s < e - 1 and arr[s] + arr[e] > 0:
         e -= 1
         answer = min(answer, abs(arr[s] + arr[e]))
+        
 print(answer)
