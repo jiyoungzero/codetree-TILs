0d49d3732d47aa0792872799ec6b2329d14ed6f8
Jiyoung Lee <79441145+jiyoungzero@users.noreply.github.com>
2024-08-10 01:20:49 +0900

    Title: 자연수 M/2개의 쌍, Time: 1437ms, Memory: 30MB, Status: Passed - CodeTree

--- 240810/자연수 M/2개의 쌍/m2-pairs-of-natural-numbers.py
+++ 240810/자연수 M/2개의 쌍/m2-pairs-of-natural-numbers.py
@@ -5,11 +5,26 @@ n = int(input())
 nums = []
 for _ in range(n):
     cnt, num = map(int, input().split())
-    for _ in range(cnt): nums.append(num)
+    nums.append([num, cnt])
 
 nums.sort()
 answer = 0
-n = len(nums)
-for i in range(n//2):
-    answer = max(answer, nums[i]+nums[n-i-1])
+
+while len(nums) > 2:
+    if len(nums) == 1:
+        answer = nums[0][0]*2
+        break
+    if len(nums) == 0:break
+    min_val, min_cnt = nums[0]
+    max_val, max_cnt = nums[-1]
+    answer = max(answer, min_val+max_val)
+    rmv = min(min_cnt, max_cnt)
+    nums[0][1] -= rmv
+    if nums[0][1] <= 0:
+        del nums[0]
+    nums[-1][1] -= rmv
+    if nums[-1][1] <= 0:
+        del nums[-1]
+ 
+    
 print(answer)
