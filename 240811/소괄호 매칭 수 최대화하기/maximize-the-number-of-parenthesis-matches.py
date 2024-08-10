6e5df675db84b55aea4c5e36f64bcfb21d121582
Jiyoung Lee <79441145+jiyoungzero@users.noreply.github.com>
2024-08-11 00:43:40 +0900

    Title: 소괄호 매칭 수 최대화하기, Time: 451ms, Memory: 32MB, Status: Passed - CodeTree

--- 240811/소괄호 매칭 수 최대화하기/maximize-the-number-of-parenthesis-matches.py
+++ 240811/소괄호 매칭 수 최대화하기/maximize-the-number-of-parenthesis-matches.py
@@ -3,8 +3,8 @@ input = sys.stdin.readline
 from functools import cmp_to_key
 
 def custom_compare(item1, item2):
-    o_cnt1, c_cnt1, _ = item1
-    o_cnt2, c_cnt2, _ = item2
+    o_cnt1, c_cnt1 = item1
+    o_cnt2, c_cnt2 = item2
     if o_cnt1 * c_cnt2 > o_cnt2 * c_cnt1:
         return -1
     elif  o_cnt1 * c_cnt2 < o_cnt2 * c_cnt1:
@@ -12,7 +12,7 @@ def custom_compare(item1, item2):
     else:
         return 0
 
-
+answer = 0
 n = int(input())
 arr = []
 for _ in range(n):
@@ -20,28 +20,16 @@ for _ in range(n):
     o_cnt, c_cnt = 0, 0
     for e in ele:
         if e == '(': o_cnt += 1
-        else: c_cnt += 1
-    arr.append((o_cnt, c_cnt, ele))
+        else: 
+            c_cnt += 1
+            answer += o_cnt
+    arr.append((o_cnt, c_cnt))
 
 
 arr.sort(key = cmp_to_key(custom_compare))
-result = ''
-for ele in arr:
-    result += ele[2]
 
-n = len(result)
-
-prefix = [0]*n
-
-prefix[0] = 1 if result[0] == ")" else 0
-for i in range(1, n):
-    if result[i] == ")":
-        prefix[i] = prefix[i-1] + 1
-    else:
-        prefix[i] = prefix[i-1]
-
-answer = 0
-for i in range(n):
-    if result[i] == "(":
-        answer += (prefix[-1]-prefix[i])
+o_sum = 0
+for o_cnt, c_cnt in arr:
+    answer += (o_sum * c_cnt)
+    o_sum += o_cnt
 print(answer)
