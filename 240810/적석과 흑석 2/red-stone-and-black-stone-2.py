f0bc341fb3ab2d0b101adcb4673b9181b5bc7c8b
Jiyoung Lee <79441145+jiyoungzero@users.noreply.github.com>
2024-08-10 22:12:51 +0900

    Title: 적석과 흑석 2, Time: 5075ms, Memory: 36MB, Status: Time Limit Exceed - CodeTree

--- 240810/적석과 흑석 2/red-stone-and-black-stone-2.py
+++ 240810/적석과 흑석 2/red-stone-and-black-stone-2.py
@@ -1,6 +1,7 @@
 import sys
 input = sys.stdin.readline 
 import heapq
+from bisect import bisect_left, bisect_right
 
 c, n = map(int, input().split())
 reds = [int(input()) for _ in range(c)]
@@ -10,14 +11,22 @@ answer = 0
 reds.sort()
 blacks.sort(key = lambda x : (x[1])) # b가 작은 값부터 보면서 a <= 를 만족하는 가장 작은 t값을 매칭
 
-heap = []
-idx = 0
+# print(reds)
+# print(blacks)
+
 for a, b in blacks:
-    while idx < c and a <= reds[idx] <= b:
-        heapq.heappush(heap, reds[idx])
+    idx = bisect_left(reds, a)
+    heap = []
+    
+    while idx < len(reds) and a <= reds[idx] <= b:
+        # print(a, b, idx)
+        heapq.heappush(heap, (reds[idx], idx))
         idx += 1
 
     if heap:
         answer += 1
+        del_idx = heap[0][1]
+        del reds[del_idx]
         heapq.heappop(heap)
+
 print(answer)
