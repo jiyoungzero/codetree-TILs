8cf1f8d98656c35bee9259f70195c2383f619c11
Jiyoung Lee <79441145+jiyoungzero@users.noreply.github.com>
2024-08-12 17:55:05 +0900

    Title: 격자 칠하기 2, Time: 394ms, Memory: 29MB, Status: Passed - CodeTree

--- 240812/격자 칠하기 2/painting-the-grid-2.py
+++ 240812/격자 칠하기 2/painting-the-grid-2.py
@@ -1,42 +1,48 @@
 import sys
 input = sys.stdin.readline 
 from collections import deque
+sys.setrecursionlimit(10000)
 
 n = int(input())
 arr= [list(map(int, input().split())) for _ in range(n)]
 
 l, r = 0, 1000000
-answer = r 
+answer = r
+visited = [[False]*n for _ in range(n)]
+dxs, dys = [0,0,1,-1], [1,-1,0,0]
+
 
 def in_range(x, y):
     return 0 <= x < n and 0 <= y < n
 
 def bfs(sx, sy, D):
-    dxs, dys = [0,0,1,-1], [1,-1,0,0]
     que = deque()
-    result = 1
     que.append((sx, sy))
-    visited = [[False]*n for _ in range(n)]
-    visited[sx][sy] = True
     
+    visited[sx][sy] = True
+    result = 1
     while que:
         x, y = que.popleft()
         for dir in range(4):
             nx, ny = x+ dxs[dir], y + dys[dir]
             if not in_range(nx, ny): continue 
+
             if not visited[nx][ny] and abs(arr[nx][ny] - arr[x][y]) <= D:
                 result += 1
                 que.append((nx, ny))
                 visited[nx][ny] = True
+                
     return result
 
 def is_possible(target): # d = target 만큼일 때, 시작점에서부터 차이가 d 이하라면 cnt + 1, cnt >= n*n//2 
     for i in range(n):
         for j in range(n):
-            cnt = bfs(i, j, target) 
-            if n%2 == 0 and cnt >= (n*n)//2:
-                return True
-            else:
+            visited[i][j] = False    
+    
+    for i in range(n):
+        for j in range(n):
+            if not visited[i][j]:
+                cnt = bfs(i, j, target) 
                 if cnt >= (n*n+1)//2:
                     return True
     return False
