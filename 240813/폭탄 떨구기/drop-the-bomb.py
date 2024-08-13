be628ef2724bfde9736e53a6c4b091751596ea3f
Jiyoung Lee <79441145+jiyoungzero@users.noreply.github.com>
2024-08-13 15:10:31 +0900

    Title: 폭탄 떨구기, Time: 142ms, Memory: 25MB, Status: Passed - CodeTree

--- 240813/폭탄 떨구기/drop-the-bomb.py
+++ 240813/폭탄 떨구기/drop-the-bomb.py
@@ -6,23 +6,19 @@ from bisect import bisect_right, bisect_left
 n, k = map(int, input().split())
 arr = [int(input()) for _ in range(n)]
 arr.sort()
-start = arr[0]
-end = arr[-1]
+
 
 def is_possible(mid):
-    global start, end
+    cnt = 1
     idx = 0
-    cnt = 0
 
-    while idx < n:
-        if cnt > k: return False
-        max_range = min(end, arr[idx]+mid*2)
-        nxt_idx = bisect_right(arr, max_range)
-
-        idx = nxt_idx
-        cnt += 1
-    if cnt > k :return False
-    return True
+    for i in range(n):
+        if arr[i] - arr[idx] <= 2*mid:
+            continue 
+        else:
+            idx = i
+            cnt += 1
+    return cnt <= k
 
 
 l, r = 0, 10**9
