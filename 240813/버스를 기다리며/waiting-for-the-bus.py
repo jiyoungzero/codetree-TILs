0f68fcfc5338eff62bd48252d788527efd40de95
Jiyoung Lee <79441145+jiyoungzero@users.noreply.github.com>
2024-08-13 15:25:53 +0900

    Title: 버스를 기다리며, Time: 147ms, Memory: 28MB, Status: Wrong Answer - CodeTree

---
+++ 240813/버스를 기다리며/waiting-for-the-bus.py
@@ -0,0 +1,33 @@
+import sys
+input = sys.stdin.readline 
+
+n, m, c = map(int, input().split())
+arr = list(map(int, input().split()))
+arr.sort()
+
+def is_possible(wait_limit):
+    bus_cnt = 1
+    idx = 0
+
+    for i in range(1, n):
+        if arr[i] - arr[idx] < wait_limit:
+            continue 
+        else:
+            idx = i 
+            bus_cnt += 1
+
+    return bus_cnt <= m
+    
+
+
+l, r = 0, 10**9
+answer = r
+while l <= r:
+    mid = (l+r)//2
+    if is_possible(mid):
+        r = mid - 1
+        answer = min(answer, mid)
+    else:
+        l = mid + 1
+
+print(answer)
