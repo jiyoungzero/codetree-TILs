1a52ef0837b9cb42c0a4716c231f1f9a583e1bb2
Jiyoung Lee <79441145+jiyoungzero@users.noreply.github.com>
2024-08-22 13:57:08 +0900

    Title: 청소는 즐거워, Time: 492ms, Memory: 28MB, Status: Passed - CodeTree

--- 240822/청소는 즐거워/cleaning-is-joyful.py
+++ 240822/청소는 즐거워/cleaning-is-joyful.py
@@ -14,54 +14,25 @@ def in_range(x, y):
 
 def swipe(sx, sy, nx, ny):
     global arr, answer
-    one_sdx, one_sdy = [-2, -1, -1, -1, 0, 1, 1, 1, 2, 0], [0, -1, 0, 1, -2, -1, 0, 1, 0, -1]
-    two_sdx, two_sdy = [0, 1, 0, -1, 2, 1, 0, -1, 0, 1], [-2, -1, -1, -1, 0, 1, 1, 1, 2, 0]
-    three_sdx, three_sdy = [-2, -1, -1, -1, 0, 1, 1, 1, 2, 0], [0, 1, 0, -1, 2, 1, 0, -1, 0, 1]
-    four_sdx, four_sdy = [0, -1, 0, 1, -2, -1, 0, 1, 0, -1], [2, 1, 1, 1, 0, -1, -1, -1, -2, 0]
+    # sdx, sdy를 2차원 배열로 만들기
+    dust_dxs_dys = [[[-2, -1, -1, -1, 0, 1, 1, 1, 2, 0], [0, -1, 0, 1, -2, -1, 0, 1, 0, -1]],
+    [[0, 1, 0, -1, 2, 1, 0, -1, 0, 1], [-2, -1, -1, -1, 0, 1, 1, 1, 2, 0]],
+    [[-2, -1, -1, -1, 0, 1, 1, 1, 2, 0], [0, 1, 0, -1, 2, 1, 0, -1, 0, 1]],
+    [[0, -1, 0, 1, -2, -1, 0, 1, 0, -1], [2, 1, 1, 1, 0, -1, -1, -1, -2, 0]]]
     prop = [0.02, 0.1, 0.07, 0.01, 0.05, 0.1, 0.07, 0.01, 0.02]
     sand = arr[nx][ny]
     for k in range(9):
-        if dir == 0:
-            nnx, nny = nx + one_sdx[k], ny + one_sdy[k]
-            move = math.floor(sand * prop[k])
-            if not in_range(nnx, nny):
-                answer += move 
-            else:
-                arr[nnx][nny] += move 
-            arr[nx][ny] -= move 
-        elif dir == 1:
-            nnx, nny = nx + two_sdx[k], ny + two_sdy[k]
-            move = math.floor(sand * prop[k])
-            if not in_range(nnx, nny):
-                answer += move 
-            else:
-                arr[nnx][nny] += move 
-            arr[nx][ny] -= move
-        elif dir == 2:
-            nnx, nny = nx + three_sdx[k], ny + three_sdy[k]
-            move = math.floor(sand * prop[k])
-            if not in_range(nnx, nny):
-                answer += move 
-            else:
-                arr[nnx][nny] += move 
-            arr[nx][ny] -= move 
+        sdx, sdy = dust_dxs_dys[dir]
+        nnx, nny = nx + sdx[k], ny + sdy[k]
+        move = math.floor(sand * prop[k])
+        if not in_range(nnx, nny):
+            answer += move 
         else:
-            nnx, nny = nx + four_sdx[k], ny + four_sdy[k]
-            move = math.floor(sand * prop[k])
-            if not in_range(nnx, nny):
-                answer += move 
-            else:
-                arr[nnx][nny] += move 
-            arr[nx][ny] -= move 
+            arr[nnx][nny] += move 
+        arr[nx][ny] -= move 
     # a% 
-    if dir == 0:
-        nnx, nny = nx + one_sdx[-1], ny + one_sdy[-1]
-    if dir == 1:
-        nnx, nny = nx + two_sdx[-1], ny + two_sdy[-1]
-    if dir == 2:
-        nnx, nny = nx + three_sdx[-1], ny + three_sdy[-1]
-    if dir == 3:
-        nnx, nny = nx + four_sdx[-1], ny + four_sdy[-1]
+    sdx, sdy = dust_dxs_dys[dir]
+    nnx, nny = nx + sdx[-1], ny + sdy[-1]
     if not in_range(nnx, nny):
         answer += arr[nx][ny]
     else:
