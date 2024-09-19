a3861979f100d23cd209fc7f488e0d5810279867
Jiyoung Lee <79441145+jiyoungzero@users.noreply.github.com>
2024-09-20 00:15:36 +0900

    Title: 가장 작은 직사각형 오리기, Time: 281ms, Memory: 36MB, Status: Wrong Answer - CodeTree

---
+++ 240920/가장 작은 직사각형 오리기/cut-the-smallest-rectangle.py
@@ -0,0 +1,66 @@
+import sys
+input = sys.stdin.readline 
+from collections import deque
+
+n = int(input())
+cmds = [tuple(map(int, input().split())) for _ in range(n)]
+cmds.sort(key = lambda x : (x[0], -x[1]))
+max_row = n
+max_col = cmds[-1][1]
+arr = [[0 ]* (max_col+1) for _ in range(max_row+1)]
+visited = [[False]*(max_col+1) for _ in range(max_row+1)]
+answer = 0
+
+for cmd in cmds:
+    s, e = cmd 
+    cur_row = 0
+    for i in range(max_row):
+        flag = True
+        cur_row = i
+        for j in range(s, e+1):
+            if arr[i][j] == 1:
+                flag = False
+                break
+        if flag:
+            for j in range(s, e+1):
+                arr[cur_row][j] = 1
+            break
+
+def in_range(x, y):
+    return 0 <= x < max_row+1 and 0 <= y < max_col+1
+
+def bfs(sx, sy):
+    dxs, dys = [0,0,1,-1], [1, -1,0, 0]
+    result = [(sx, sy)]
+    que = deque()
+    que.append((sx, sy))
+    visited[sx][sy] = True 
+
+    while que:
+        x, y = que.popleft()
+        for dir in range(4):
+            nx, ny = x + dxs[dir], y + dys[dir]
+            if not in_range(nx, ny):continue 
+            if not visited[nx][ny] and arr[nx][ny] == 1:
+                result.append((nx, ny))
+                visited[nx][ny] = True 
+                que.append((nx, ny))
+    return result
+
+for i in range(max_row+1):
+    for j in range(max_col+1):
+        if arr[i][j] == 1 and not visited[i][j]:
+            result = bfs(i, j)
+            # 제일 작은 행 
+            # 제일 큰 행 
+            result.sort(key = lambda x : -x[0])
+            max_r, min_r = result[0][0], result[-1][0]
+
+            # 제일 작은 열, 제일 큰 열
+            result.sort(key = lambda x : -x[1])
+            max_c, min_c = result[0][1], result[-1][1]
+
+            total = (max_r - min_r + 1) * (max_c - min_c + 1)
+            answer += (total)
+
+print(answer)
