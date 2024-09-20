3450e2df8795e1ac4de4d0b48b9f461f08bd469c
Jiyoung Lee <79441145+jiyoungzero@users.noreply.github.com>
2024-09-20 18:41:25 +0900

    Title: 블럭쌓는 명령, Time: 196ms, Memory: 73MB, Status: Passed - CodeTree

--- 240920/블럭쌓는 명령/block-stacking-commands.py
+++ 240920/블럭쌓는 명령/block-stacking-commands.py
@@ -3,12 +3,12 @@ input = sys.stdin.readline
 
 n, k = map(int, input().split())
 cmds = [tuple(map(int, input().split())) for _ in range(k)]
-arr = [0]*(n+1)
+arr = [0]*(n+2)
 for s, e in cmds:
     arr[s] += 1
     arr[e+1] -= 1
-for i in range(1, n+1):
+for i in range(1, n+2):
     arr[i] += arr[i-1] 
-
+arr = arr[2:]
 arr.sort()
-print(arr[len(arr)//2])
+print(arr[n//2])
