2eed03050de18777dd55a5e4e17a974ef2bcc127
Jiyoung Lee <79441145+jiyoungzero@users.noreply.github.com>
2024-09-28 12:27:40 +0900

    Title: 굉장한 수 만들기, Time: 259ms, Memory: 28MB, Status: Passed - CodeTree

--- 240928/굉장한 수 만들기/create-an-awesome-number.py
+++ 240928/굉장한 수 만들기/create-an-awesome-number.py
@@ -7,16 +7,33 @@ def factorial(num):
     return factorial(num-1) + num 
 
 n = int(input())
-nums = [factorial(num) for num in range(1, 701)]
-boxes = [0]*(700)
+nums = [factorial(num) for num in range(1, 751)]
+boxes = [0]*(750)
 boxes[0] = 1
-for i in range(1, 700):
+for i in range(1, 750):
     boxes[i] = boxes[i-1] + nums[i]
 
 answer = 0
-# greedy 
-for box in boxes[::-1]:
-    if box <= n:
-        answer += (n//box)
-        n %= box 
-print(answer)
+# dp 
+dp = [0]*(n+1)
+cur = 0
+dp[1] = 1
+for i in range(2, n+1):
+    if boxes[cur+1] > i:
+        min_value = int(1e9)
+        for j in range(cur+1):
+            min_value = min(min_value, dp[i-boxes[j]] + 1)
+        dp[i] = min_value
+
+    # cur의 값이 i보다 같거나 클 때 
+    else:
+        cur += 1
+        if boxes[cur] == i:
+            dp[i] = 1
+        else:
+            min_value = int(1e9)
+            for j in range(cur+1):
+                min_value = min(min_value, dp[i-boxes[j]] + 1)
+            dp[i] = min_value
+# print(boxes[:6])
+print(dp[n])
