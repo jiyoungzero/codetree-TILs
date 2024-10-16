3341301faffbfece6b85d54b38155bb368ac2494
Jiyoung Lee <79441145+jiyoungzero@users.noreply.github.com>
2024-10-16 16:39:07 +0900

    Title: 맞닿지 않은 사각형 선택, Time: 166ms, Memory: 35MB, Status: Passed - CodeTree

--- 241016/맞닿지 않은 사각형 선택/Select-non-contact-rectangles.py
+++ 241016/맞닿지 않은 사각형 선택/Select-non-contact-rectangles.py
@@ -2,8 +2,9 @@ n = int(input())
 arr = [list(map(int, input().split())) for _ in range(2)]
 dp = [[0]*n for _ in range(2)]
 
-dp[0][0], dp[0][1] = arr[0][0], arr[0][1]
+dp[0][0], dp[1][0] = arr[0][0], arr[1][0]
 for j in range(1, n):
     dp[0][j] = max(dp[1][j-1] + arr[0][j], dp[0][j-1])
     dp[1][j] = max(dp[0][j-1] + arr[1][j], dp[1][j-1])
+
 print(max(dp[0][-1], dp[1][-1]))
