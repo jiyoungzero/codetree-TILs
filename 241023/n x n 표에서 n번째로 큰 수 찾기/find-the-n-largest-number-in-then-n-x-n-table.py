639162ec07d39b93f1739721cdaf0c03725794b7
Jiyoung Lee <79441145+jiyoungzero@users.noreply.github.com>
2024-10-23 15:38:25 +0900

    Title: n x n 표에서 n번째로 큰 수 찾기, Time: 5091ms, Memory: 34MB, Status: Time Limit Exceed - CodeTree

--- 241023/n x n 표에서 n번째로 큰 수 찾기/find-the-n-largest-number-in-then-n-x-n-table.py
+++ 241023/n x n 표에서 n번째로 큰 수 찾기/find-the-n-largest-number-in-then-n-x-n-table.py
@@ -6,9 +6,12 @@ nums = list(map(int, input().split()))
 heap = []
 for i in range(n):
     for j in range(n):
-        heapq.heappush(heap, -int(nums[i]/(j+1)))
+        if len(heap) < n:
+            heapq.heappush(heap, int(nums[i]/(j+1)))
+        else:
+            if heap[0] < int(nums[i]/(j+1)):
+                heapq.heappop(heap)
+                heapq.heappush(heap, int(nums[i]/(j+1)))
 
 
-for i in range(n-1):
-    heapq.heappop(heap)
-print(-heap[0])
+print(heap[0])
