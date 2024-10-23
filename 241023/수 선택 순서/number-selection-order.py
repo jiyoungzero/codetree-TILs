c3f0770f98be62caac0798e3834c1399b1311ce9
Jiyoung Lee <79441145+jiyoungzero@users.noreply.github.com>
2024-10-23 15:04:20 +0900

    Title: 수 선택 순서, Time: 161ms, Memory: 25MB, Status: Passed - CodeTree

--- 241023/수 선택 순서/number-selection-order.py
+++ 241023/수 선택 순서/number-selection-order.py
@@ -1,21 +1,36 @@
-import heapq
+import heapq 
 
 n, x = map(int, input().split())
-heap = []
 nums = list(map(int, input().split()))
-answer = 1
-for num in nums:
+heap = []
+num2idx = []
+for i, num in enumerate(nums):
     heapq.heappush(heap, -num)
+    num2idx.append((num, i))
 
-while nums:
-    ele = nums.pop(0)
-    if ele == x:
-        break 
-    elif ele == -heap[0]:
+answer = 1
+while num2idx:
+    top = heap[0]
+    num, idx = num2idx[0]
+    # print(top, num, idx)
+
+    # 현재 nums의 앞의 값이 heapq 앞 데이터와 같으며, num_idx와 x가 값이 같을 때 break 
+    if num == -top and idx == x:
+        break
+    # 현재 nums의 앞의 값이 heapq 앞 데이터와 같을 때
+    elif num == -top:
+        # 1. nums.pop(0),
+        num2idx.pop(0) 
+        # 2. heapq.heappop(heap)
         heapq.heappop(heap)
-    else:
-        nums.append(ele)
-    answer += 1
-
-
+        # 3. answer += 1
+        answer += 1
+    # 현재 nums의 앞이 값과 heapq 앞 데이터가 다를 때 
+    elif num != -top:
+        # 1. nums.pop(0) -> nums.append(ele)
+        num, idx = num2idx.pop(0)
+        num2idx.append((num, idx))
+        # 2. answer += 1
+        # answer += 1
+    # print(answer)
 print(answer)
