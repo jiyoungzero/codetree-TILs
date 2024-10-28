36e7b9cd5b815155a7ac32906f828390a70ab7d9
Jiyoung Lee <79441145+jiyoungzero@users.noreply.github.com>
2024-10-28 23:53:10 +0900

    Title: 체크판위에서, Time: 1580ms, Memory: 28MB, Status: Passed - CodeTree

--- 241028/체크판위에서/on-the-checkboard.py
+++ 241028/체크판위에서/on-the-checkboard.py
@@ -2,6 +2,7 @@ r, c = map(int, input().split())
 arr = [input() for _ in range(r)]
 dxs, dys = [1, 0], [0, 1] # 아래, 오른쪽 
 answer = 0
+visited = [[False]*c for _ in range(r)]
 
 def backtracking(cur):
     global answer
@@ -16,9 +17,11 @@ def backtracking(cur):
         for j in range(1, c):
             nx = x + dxs[0]*i
             ny = y + dys[1]*j
-            if nx < r and ny < c and arr[x][y] != arr[nx][ny]:
+            if nx < r and ny < c and not visited[nx][ny]  and arr[x][y] != arr[nx][ny]:
+                visited[nx][ny] = True
                 backtracking((x + dxs[0]*i,  y+ dys[1]*j))
+                visited[nx][ny] = False
 
-
+visited[0][0] = True
 backtracking((0, 0))
 print(answer)
