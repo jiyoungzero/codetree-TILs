44733602cb9758496778d23687862efaeae44cd9
Jiyoung Lee <79441145+jiyoungzero@users.noreply.github.com>
2024-10-31 22:38:17 +0900

    Title: LED 전구 버튼, Time: 5061ms, Memory: 25MB, Status: Time Limit Exceed - CodeTree

---
+++ 241031/LED 전구 버튼/led-bulb-button.py
@@ -0,0 +1,22 @@
+n, b = map(int, input().split())
+lights = [int(input()) for _ in range(n)]
+# 왼쪽 : 인덱스 - 1 (if (idx - 1) < 0 : check_idx = -1)
+for _ in range(b):
+    nxt_lights = []
+    for i in range(n):
+        check_idx = i - 1
+        if check_idx < 0:
+            check_idx = -1
+            if lights[check_idx] == 1:
+                nxt_lights.append((lights[i]+1)%2)
+            else:
+                nxt_lights.append(lights[i])
+        else:
+            if lights[check_idx] == 1:
+                nxt_lights.append((lights[i]+1)%2)
+            else:
+                nxt_lights.append(lights[i])
+    lights = nxt_lights[:]
+
+for ele in lights:
+    print(ele)
