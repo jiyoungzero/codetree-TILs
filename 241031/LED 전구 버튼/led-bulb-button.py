da20848cccf795d3c89dba5ca40c6bb33e071943
Jiyoung Lee <79441145+jiyoungzero@users.noreply.github.com>
2024-10-31 23:13:20 +0900

    Title: LED 전구 버튼, Time: 114ms, Memory: 24MB, Status: Passed - CodeTree

--- 241031/LED 전구 버튼/led-bulb-button.py
+++ 241031/LED 전구 버튼/led-bulb-button.py
@@ -1,23 +1,43 @@
-n, b = map(int, input().split())
-lights = [int(input()) for _ in range(n)]
-# 왼쪽 : 인덱스 - 1 (if (idx - 1) < 0 : check_idx = -1)
-max_b = 2**n
-for _ in range(b%max_b):
-    nxt_lights = []
-    for i in range(n):
-        check_idx = i - 1
-        if check_idx < 0:
-            check_idx = -1
-            if lights[check_idx] == 1:
-                nxt_lights.append((lights[i]+1)%2)
-            else:
-                nxt_lights.append(lights[i])
-        else:
-            if lights[check_idx] == 1:
-                nxt_lights.append((lights[i]+1)%2)
-            else:
-                nxt_lights.append(lights[i])
-    lights = nxt_lights[:]
+def toggle_lights(n, b, lights):
+    seen_states = {}
+    current_state = tuple(lights)
 
-for ele in lights:
-    print(ele)
+    for step in range(b):
+        if current_state in seen_states:
+            # 주기를 발견
+            cycle_length = step - seen_states[current_state]
+            remaining_steps = (b - step) % cycle_length
+            
+            for _ in range(remaining_steps):
+                new_state = []
+                for i in range(n):
+                    left_index = (i - 1) % n
+                    if current_state[left_index] == 1:
+                        new_state.append(1 - current_state[i])
+                    else:
+                        new_state.append(current_state[i])
+                current_state = tuple(new_state)
+            break
+        else:
+            seen_states[current_state] = step
+            new_state = []
+            for i in range(n):
+                left_index = (i - 1) % n
+                if current_state[left_index] == 1:
+                    new_state.append(1 - current_state[i])
+                else:
+                    new_state.append(current_state[i])
+            current_state = tuple(new_state)
+    
+    return list(current_state)
+
+# 입력 받기
+n, b = map(int, input().split())
+lights = [int(input().strip()) for _ in range(n)]
+
+# 전구 상태 변경
+final_state = toggle_lights(n, b, lights)
+
+# 결과 출력
+for state in final_state:
+    print(state)
